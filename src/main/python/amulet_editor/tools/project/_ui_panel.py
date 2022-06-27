a512be6089a4349f8e397cbd5cf41ab1ebdcc811
Jevex <79948076+jevexendo@users.noreply.github.com>
2022-06-27 00:15:36 -0500

    Clean imports (#2)

--- src/main/python/amulet_editor/tools/project/_ui_panel.py
+++ src/main/python/amulet_editor/tools/project/_ui_panel.py
@@ -8,73 +8,33 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    Qt,
-    QTime,
-    QUrl,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QFrame,
-    QHBoxLayout,
-    QHeaderView,
-    QSizePolicy,
-    QTreeView,
-    QVBoxLayout,
-    QWidget,
-)
+from PySide6.QtCore import QCoreApplication, QMetaObject, QSize
+from PySide6.QtWidgets import QFrame, QHBoxLayout, QTreeView, QVBoxLayout
 
 
 class Ui_ExplorerPanel(object):
     def setupUi(self, ExplorerPanel):
         if not ExplorerPanel.objectName():
-            ExplorerPanel.setObjectName(u"ExplorerPanel")
+            ExplorerPanel.setObjectName("ExplorerPanel")
         ExplorerPanel.resize(300, 480)
         self.verticalLayout = QVBoxLayout(ExplorerPanel)
         self.verticalLayout.setSpacing(0)
-        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setObjectName("verticalLayout")
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.frm_directory = QFrame(ExplorerPanel)
-        self.frm_directory.setObjectName(u"frm_directory")
+        self.frm_directory.setObjectName("frm_directory")
         self.frm_directory.setMinimumSize(QSize(0, 24))
         self.frm_directory.setFrameShape(QFrame.NoFrame)
         self.frm_directory.setFrameShadow(QFrame.Raised)
         self.horizontalLayout = QHBoxLayout(self.frm_directory)
         self.horizontalLayout.setSpacing(0)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.horizontalLayout.setContentsMargins(9, 9, 9, 9)
 
         self.verticalLayout.addWidget(self.frm_directory)
 
         self.trv_directory = QTreeView(ExplorerPanel)
-        self.trv_directory.setObjectName(u"trv_directory")
+        self.trv_directory.setObjectName("trv_directory")
         self.trv_directory.setFrameShape(QFrame.NoFrame)
         self.trv_directory.setIndentation(8)
         self.trv_directory.setUniformRowHeights(True)
@@ -91,7 +51,7 @@ class Ui_ExplorerPanel(object):
 
     def retranslateUi(self, ExplorerPanel):
         ExplorerPanel.setWindowTitle(
-            QCoreApplication.translate("ExplorerPanel", u"Form", None)
+            QCoreApplication.translate("ExplorerPanel", "Form", None)
         )
 
     # retranslateUi
