84c9cac9977e7e412076798f3d2132e46a7a5a98
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-23 10:48:13 +0100

    Renamed third party plugin

--- plugin_prototype/plugins/my_name.my_plugin1/_plugin.py
+++ plugin_prototype/plugins/my_name.my_plugin1/_plugin.py
@@ -1,10 +1,10 @@
 from amulet_editor_plugin_test.plugin import Plugin
 
 
-class EditorPlugin(Plugin):
+class MyPlugin(Plugin):
     def on_load(self):
-        print("Plugin Loaded")
-        editor = self.get_plugin("amulet_team.editor")
+        print("Third Party Plugin Loaded")
+        editor: EditorPlugin = self.get_plugin("amulet_team.editor")
         editor.custom_method("test")
 
     def on_unload(self):
