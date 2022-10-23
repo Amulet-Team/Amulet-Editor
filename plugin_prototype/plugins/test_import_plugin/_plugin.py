ddda34127aecaa554f18fbe8574c32b52181f7d7
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-23 11:34:43 +0100

    Added a plugin to test the import blocking mechanic

---
+++ plugin_prototype/plugins/test_import_plugin/_plugin.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+from contextlib import contextmanager
+
+from amulet_editor_plugin_test.plugin import Plugin
+
+
+@contextmanager
+def import_test():
+    try:
+        yield
+    except ImportError:
+        pass
+    else:
+        raise Exception("This should not happen")
+
+
+with import_test():
+    from amulet_team_editor.module_test import module_noop
+
+with import_test():
+    from amulet_team_editor import Plugin as EditorPlugin
+
+with import_test():
+    from my_name_my_plugin1 import Plugin as MyPlugin1
+
+
+class TestPlugin(Plugin):
+    def on_load(self):
+        print("Test Import Plugin Loaded")
+
+    def on_unload(self):
+        print("Test Import Plugin Unloaded")
