32d78f03c79f43f7cdbf825f2a5f4b83a7355670
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-23 11:33:25 +0100

    Refactored and added typing

--- plugin_prototype/plugins/my_name_my_plugin1/_plugin.py
+++ plugin_prototype/plugins/my_name_my_plugin1/_plugin.py
@@ -1,13 +1,20 @@
+from __future__ import annotations
 from typing import TYPE_CHECKING
 from amulet_editor_plugin_test.plugin import Plugin
 
 
+if TYPE_CHECKING:
+    # If you want type checking in your IDE you can do this.
+    # TYPE_CHECKING is False at runtime so this code will never get run, but it allows the IDE to know the API of the object.
+    # Note: you should also have `from __future__ import annotations` as the first line in the file.
+    from amulet_team_editor import Plugin as EditorPlugin
+
 
 class MyPlugin(Plugin):
     def on_load(self):
-        print("Third Party Plugin Loaded")
-        editor: EditorPlugin = self.get_plugin("amulet_team.editor")
+        print("My Plugin1 Loaded")
+        editor: EditorPlugin = self.get_plugin("amulet_team_editor")
         editor.custom_method("test")
 
     def on_unload(self):
-        print("Plugin Unloaded")
+        print("My Plugin1 Unloaded")
