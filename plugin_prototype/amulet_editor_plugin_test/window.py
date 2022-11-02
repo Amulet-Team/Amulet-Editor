29ef47ae690a64eef7aafef920161baeb9d1b069
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-11-02 12:04:30 +0000

    Refactored the plugin GUI
    Replaced the table with a grid layout.
    This allows the events to be bound directly to the button to give finer control.
    Also moved the activation to a thread so that the GUI does not get locked up.

--- plugin_prototype/amulet_editor_plugin_test/window.py
+++ plugin_prototype/amulet_editor_plugin_test/window.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
-from PySide6.QtCore import QCoreApplication, QMetaObject, Qt, Slot
-from PySide6.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget
-from PySide6.QtGui import QColor
+from PySide6.QtCore import QCoreApplication, QMetaObject, Qt, Slot, QThread
+from PySide6.QtWidgets import QMainWindow, QGridLayout, QVBoxLayout, QWidget, QLabel, QCheckBox
 
 from .plugin_manager import PluginData, PluginState, PluginUID
 
@@ -10,23 +9,23 @@ if TYPE_CHECKING:
     from .plugin_api import AppPrivateAPI
 
 
-EnabledState = {
-    PluginState.Disabled: Qt.Unchecked,
-    PluginState.Inactive: Qt.Checked,
-    PluginState.Enabled: Qt.Checked
-}
+class Thread(QThread):
+    def __init__(self, *QThread_args, target=None, args=(), kwargs=(), **QThread_kwargs):
+        super().__init__(*QThread_args, **QThread_kwargs)
+        self.__target = target
+        self.__args = args
+        self.__kwargs = dict(kwargs)
+        self.finished.connect(lambda: self.deleteLater())
 
+    def __del__(self):
+        print("deleted")
 
-class TableCheckboxItem(QTableWidgetItem):
-    plugin_data: PluginData
-
-    def __init__(self, plugin_data: PluginData):
-        super().__init__()
-        self.plugin_data = plugin_data
+    def run(self):
+        self.__target(*self.__args, **self.__kwargs)
 
 
 class MainWindow(QMainWindow):
-    __plugin_map: dict[PluginUID, tuple[TableCheckboxItem, QTableWidgetItem]] = {}
+    __plugin_map: dict[PluginUID, tuple[QCheckBox, QLabel]] = {}
 
     def __init__(self, api: AppPrivateAPI):
         super().__init__()
@@ -34,9 +33,7 @@ class MainWindow(QMainWindow):
         self.setObjectName("MainWindow")
         self.resize(875, 702)
         self.centralwidget = QWidget(self)
-        self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QVBoxLayout(self.centralwidget)
-        self.verticalLayout.setObjectName("verticalLayout")
         self.__init_table(self.centralwidget, self.verticalLayout)
         for plugin_data, plugin_state in self.__api.iter_plugins():
             self.__add_row(plugin_data, plugin_state)
@@ -47,77 +44,82 @@ class MainWindow(QMainWindow):
 
     def __init_table(self, parent, layout):
         self.__plugin_map = {}
-        self.tableWidget = QTableWidget(parent)
-        self.tableWidget.setSortingEnabled(False)
-        self.tableWidget.setColumnCount(5)
-        self.tableWidget.setHorizontalHeaderItem(0, QTableWidgetItem())
-        self.tableWidget.setHorizontalHeaderItem(1, QTableWidgetItem())
-        self.tableWidget.setHorizontalHeaderItem(2, QTableWidgetItem())
-        self.tableWidget.setHorizontalHeaderItem(3, QTableWidgetItem())
-        self.tableWidget.setHorizontalHeaderItem(4, QTableWidgetItem())
-        self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.itemChanged.connect(self.__on_change)
-        layout.addWidget(self.tableWidget)
-
-    def __on_change(self, item: QTableWidgetItem):
-        if isinstance(item, TableCheckboxItem):
-            if item.checkState() == Qt.Checked:
-                self.__api.enable_plugin(item.plugin_data.uid)
-            else:
-                self.__api.disable_plugin(item.plugin_data.uid)
+        self.gridLayout = QGridLayout()
+        layout.addLayout(self.gridLayout)
+        self.plugin_name_label = QLabel(parent)
+        self.gridLayout.addWidget(self.plugin_name_label, 0, 0)
+        self.plugin_id_label = QLabel(parent)
+        self.gridLayout.addWidget(self.plugin_id_label, 0, 1)
+        self.plugin_version_label = QLabel(parent)
+        self.gridLayout.addWidget(self.plugin_version_label, 0, 2)
+        self.plugin_enabled_label = QLabel(parent)
+        self.gridLayout.addWidget(self.plugin_enabled_label, 0, 3)
+        self.plugin_status_label = QLabel(parent)
+        self.gridLayout.addWidget(self.plugin_status_label, 0, 4)
 
     def __add_row(self, plugin_data: PluginData, plugin_state: PluginState):
-        index = self.tableWidget.rowCount()
-        self.tableWidget.setRowCount(index + 1)
-        self.tableWidget.setVerticalHeaderItem(index, QTableWidgetItem())
+        index = len(self.__plugin_map) + 1
 
         # plugin name
-        name_item = QTableWidgetItem()
+        name_item = QLabel(self.centralwidget)
         name_item.setText(plugin_data.name)
-        self.tableWidget.setItem(index, 0, name_item)
+        self.gridLayout.addWidget(name_item, index, 0)
 
         # plugin id
-        id_item = QTableWidgetItem()
+        id_item = QLabel(self.centralwidget)
         id_item.setText(plugin_data.uid.identifier)
-        self.tableWidget.setItem(index, 1, id_item)
+        self.gridLayout.addWidget(id_item, index, 1)
 
         # plugin version
-        version_item = QTableWidgetItem()
+        version_item = QLabel(self.centralwidget)
         version_item.setText(str(plugin_data.uid.version))
-        self.tableWidget.setItem(index, 2, version_item)
+        self.gridLayout.addWidget(version_item, index, 2)
 
         # enabled checkbox
-        enabled_item = TableCheckboxItem(plugin_data)
-        enabled_item.setCheckState(EnabledState[plugin_state])
-        self.tableWidget.setItem(index, 3, enabled_item)
+        enabled_item = QCheckBox(self.centralwidget)
+        enabled_item.setChecked(bool(plugin_state))
+        self.gridLayout.addWidget(enabled_item, index, 3)
 
         # plugin state
-        state_item = QTableWidgetItem()
+        state_item = QLabel(self.centralwidget)
         self.__set_enabled_state(state_item, plugin_state)
-        self.tableWidget.setItem(index, 4, state_item)
+        self.gridLayout.addWidget(state_item, index, 4)
 
         self.__plugin_map[plugin_data.uid] = (enabled_item, state_item)
+
+        def on_change(state: int):
+            if state == Qt.Unchecked:
+                print("evt disable", self, plugin_data.uid)
+                Thread(target=lambda : self.__api.disable_plugin(plugin_data.uid)).start()
+            else:
+                print("evt enable", self, plugin_data.uid)
+                Thread(target=lambda: self.__api.enable_plugin(plugin_data.uid)).start()
+
+        enabled_item.stateChanged.connect(on_change)
+
+        self.setFixedSize(self.width(), self.verticalLayout.sizeHint().height())
 
     @Slot(PluginUID, PluginState)
     def __on_plugin_state_change(self, plugin_uid: PluginUID, plugin_state: PluginState):
         print("state change", plugin_uid, plugin_state)
         enabled_item, state_item = self.__plugin_map[plugin_uid]
-        enabled_item.setCheckState(EnabledState[plugin_state])
+        enabled_state = bool(plugin_state)
+        if enabled_item.isChecked() != enabled_state:
+            enabled_item.setChecked(enabled_state)
         self.__set_enabled_state(state_item, plugin_state)
 
-    def __set_enabled_state(self, item: QTableWidgetItem, plugin_state: PluginState):
-        text, colour = {
-            PluginState.Disabled: ("Disabled", QColor(255, 0, 0)),
-            PluginState.Inactive: ("Inactive", QColor(255, 255, 0)),
-            PluginState.Enabled: ("Enabled", QColor(0, 255, 0))
+    def __set_enabled_state(self, item: QLabel, plugin_state: PluginState):
+        text = {
+            PluginState.Disabled: "Disabled",
+            PluginState.Inactive: "Inactive",
+            PluginState.Enabled: "Enabled"
         }[plugin_state]
         item.setText(text)
-        item.setBackground(colour)
 
     def translate(self):
         self.setWindowTitle(QCoreApplication.translate("MainWindow", "MainWindow", None))
-        self.tableWidget.horizontalHeaderItem(0).setText(QCoreApplication.translate("MainWindow", "Plugin Name", None))
-        self.tableWidget.horizontalHeaderItem(1).setText(QCoreApplication.translate("MainWindow", "Plugin Identifier", None))
-        self.tableWidget.horizontalHeaderItem(2).setText(QCoreApplication.translate("MainWindow", "Plugin Version", None))
-        self.tableWidget.horizontalHeaderItem(3).setText(QCoreApplication.translate("MainWindow", "Plugin Enabled", None))
-        self.tableWidget.horizontalHeaderItem(4).setText(QCoreApplication.translate("MainWindow", "Plugin State", None))
+        self.plugin_name_label.setText(QCoreApplication.translate("MainWindow", "Plugin Name", None))
+        self.plugin_id_label.setText(QCoreApplication.translate("MainWindow", "Plugin Identifier", None))
+        self.plugin_version_label.setText(QCoreApplication.translate("MainWindow", "Plugin Version", None))
+        self.plugin_enabled_label.setText(QCoreApplication.translate("MainWindow", "Plugin Enabled", None))
+        self.plugin_status_label.setText(QCoreApplication.translate("MainWindow", "Plugin State", None))
