cf95f4985983ffb1a1fc22fd14f83d3a6018cd6e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-11-24 16:29:01 +0000

    Added a tool to build pyproject file
    Qt creator has a json file to list the paths of all files to display.
    It can however get out of sync with the true source.
    This script scans the directory for all ui files and rebuilds the list.

---
+++ tools/build_pyproject.py
@@ -0,0 +1,24 @@
+"""
+Qt Creator requires a JSON file to define all files that it can see.
+This will probably get out of sync at some point so this program will generate it.
+"""
+
+import glob
+import os
+import json
+
+ProjectRoot = os.path.dirname(os.path.dirname(__file__))
+
+
+def main():
+    paths = []
+    for path in glob.glob(os.path.join(ProjectRoot, "src", "**", "*.ui"), recursive=True):
+        if os.path.isfile(path):
+            paths.append(os.path.relpath(path, ProjectRoot))
+
+    with open(os.path.join(os.path.dirname(__file__), "..", "Amulet-Editor.pyproject"), "w") as f:
+        json.dump({"files": paths}, f, indent="\t")
+
+
+if __name__ == '__main__':
+    main()
