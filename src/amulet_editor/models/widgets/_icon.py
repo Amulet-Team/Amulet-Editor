10ca1899e0ec395c9394998be444ec1adf3a9eee
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-11-26 10:32:32 +0000

    Removed widgets
    Removed QLinkCard, QDragContainer, QDragIconButton, QIconButton, QSvgIcon, QIconCard

--- src/amulet_editor/models/widgets/_icon.py
+++ src/amulet_editor/models/widgets/_icon.py
@@ -1,81 +1,11 @@
 from typing import Optional
-import warnings
 
 from amulet_editor.data import build
-from amulet_editor.data.build import get_resource
 from amulet_editor.models.widgets._label import QHoverLabel
-from PySide6 import QtGui
 from PySide6.QtCore import QEvent, QSize, Qt
-from PySide6.QtGui import QColor, QEnterEvent, QIcon, QPainter, QPaintEvent, QImage
-from PySide6.QtWidgets import QToolButton, QWidget, QStyleOption, QStyle, QVBoxLayout, QPushButton
+from PySide6.QtGui import QColor, QEnterEvent, QPainter, QPaintEvent, QImage
+from PySide6.QtWidgets import QWidget, QStyleOption, QStyle, QVBoxLayout, QPushButton
 from PySide6.QtSvgWidgets import QSvgWidget
-
-
-class QSvgIcon(QIcon):
-    def __init__(self, filename: str, size: QSize, color: QColor) -> None:
-        warnings.warn("QSvgIcon is depreciated. Use AStylableSvgWidget instead", DeprecationWarning)
-        pixmap = QIcon(filename).pixmap(size)
-
-        painter = QPainter(pixmap)
-        painter.setCompositionMode(QPainter.CompositionMode_SourceIn)
-        painter.setBrush(color)
-        painter.setPen(color)
-        painter.drawRect(pixmap.rect())
-        painter.end()
-
-        super().__init__(pixmap)
-
-
-class QIconButton(QToolButton):
-    def __init__(self, parent: QWidget) -> None:
-        warnings.warn("QIconButton is depreciated. Use AIconButton instead", DeprecationWarning)
-        super().__init__(parent)
-
-        self._icon_name = "question-mark"
-        self._icon_color = None
-
-        self._hlbl_tooltip = QHoverLabel("", self)
-        self._hlbl_tooltip.hide()
-
-    def setIcon(self, icon_name: Optional[str] = None) -> None:
-        if icon_name is not None:
-            self._icon_name = icon_name
-
-        icon_color = self._icon_color
-        self._icon_color = (
-            self.palette().brightText().color()
-            if self.isChecked()
-            else self.palette().text().color()
-        )
-
-        if icon_color != self._icon_color:
-            super().setIcon(
-                QSvgIcon(
-                    get_resource(f"icons/tabler/{self._icon_name}"),
-                    self.iconSize(),
-                    self._icon_color,
-                )
-            )
-            self.parent().update()  # Fix rendering artifacts
-
-    def toolTip(self) -> QHoverLabel:
-        return self._hlbl_tooltip
-
-    def setToolTip(self, label: QHoverLabel) -> None:
-        self._hlbl_tooltip = label
-
-    def event(self, event: QEvent) -> bool:
-        if type(event) == QtGui.QPaintEvent:
-            self.setIcon()
-
-        return super().event(event)
-
-    def enterEvent(self, event: QEnterEvent):
-        if len(self._hlbl_tooltip.text()) > 0:
-            self._hlbl_tooltip.show()
-
-    def leaveEvent(self, event: QEvent):
-        self._hlbl_tooltip.hide()
 
 
 class AStylableSvgWidget(QSvgWidget):
--- src/amulet_editor/tools/startup/_widgets.py
+++ src/amulet_editor/tools/startup/_widgets.py
@@ -1,12 +1,8 @@
 import pathlib
 from typing import Optional
-import warnings
 
-from amulet_editor.application import appearance
-from amulet_editor.application.appearance import Color, Theme
 from amulet_editor.data import build
 from amulet_editor.models.minecraft import LevelData
-from amulet_editor.models.widgets._icon import QSvgIcon
 from amulet_editor.models.widgets import AStylableSvgWidget
 from amulet_editor.models.widgets import QElidedLabel
 from PySide6.QtCore import QCoreApplication, QEvent, QSize, Qt
@@ -19,117 +15,8 @@ from PySide6.QtWidgets import (
     QPushButton,
     QSizePolicy,
     QStackedWidget,
-    QToolButton,
     QWidget,
 )
-
-
-class QIconButton(QToolButton):
-    def __init__(self, parent: QWidget) -> None:
-        warnings.warn("QIconButton is depreciated. Use AIconButton instead", DeprecationWarning)
-        super().__init__(parent)
-
-        self._icon_name = "question-mark"
-
-        appearance.changed.connect(self.retheme)
-
-    def setIcon(self, icon_name: Optional[str] = None) -> None:
-        self._icon_name = icon_name
-
-        self.repaint(appearance.theme().on_surface)
-
-    def repaint(self, color: Color) -> None:
-        super().setIcon(
-            QSvgIcon(
-                build.get_resource(f"icons/tabler/{self._icon_name}"),
-                self.iconSize(),
-                color.get_qcolor(),
-            )
-        )
-        self.setStyleSheet(f"color: {color.get_hex()}")
-
-    def retheme(self, theme: Theme) -> None:
-        self.repaint(theme.on_surface)
-
-    def enterEvent(self, event: QEnterEvent) -> None:
-        self.repaint(appearance.theme().on_primary)
-        return super().enterEvent(event)
-
-    def leaveEvent(self, event: QEvent) -> None:
-        if self.isCheckable() and not self.isChecked():
-            self.repaint(appearance.theme().on_surface)
-
-        return super().leaveEvent(event)
-
-
-class QIconCard(QPushButton):
-    def __init__(
-        self,
-        icon: str,
-        icon_size: QSize,
-        parent: Optional[QWidget] = None,
-    ) -> None:
-        super().__init__(parent=parent)
-
-        self.icon_path = icon
-        self.icon_size = icon_size
-
-        self.lbl_icon = QLabel()
-        self.lbl_description = QElidedLabel()
-
-        self.lbl_icon.setAttribute(Qt.WA_TransparentForMouseEvents)
-        self.lbl_icon.setSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
-
-        self.lbl_description.setAttribute(Qt.WA_TransparentForMouseEvents)
-        self.lbl_description.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Maximum)
-
-        layout = QHBoxLayout()
-        layout.addWidget(self.lbl_icon)
-        layout.addWidget(self.lbl_description)
-        layout.setAlignment(Qt.AlignCenter)
-        layout.setContentsMargins(5, 5, 5, 5)
-        layout.setSpacing(5)
-
-        self.setLayout(layout)
-        self.setMinimumHeight(self.icon_size.height() + 10)
-
-        self.repaint(appearance.theme().on_surface)
-
-        appearance.changed.connect(self.retheme)
-
-    def setText(self, text: str, heading: Optional[str]):
-        if heading is not None:
-            self.lbl_description.setProperty("heading", heading)
-
-        self.lbl_description.setProperty("subfamily", "semi_light")
-        self.lbl_description.setText(text)
-
-    def setIcon(self, icon_path: str) -> None:
-        if self.icon_path is None:
-            layout: QHBoxLayout = self.layout()
-            layout.insertWidget(0, self.lbl_icon)
-        self.icon_path = icon_path
-        self.repaint(appearance.theme().on_surface)
-
-    def repaint(self, color: Color) -> None:
-        if self.icon_path is not None:
-            self.lbl_icon.setPixmap(
-                QSvgIcon(self.icon_path, self.icon_size, color.get_qcolor()).pixmap(
-                    self.icon_size
-                )
-            )
-        self.setStyleSheet(f"color: {color.get_hex()}")
-
-    def retheme(self, theme: Theme) -> None:
-        self.repaint(theme.on_surface)
-
-    def enterEvent(self, event: QEnterEvent) -> None:
-        self.repaint(appearance.theme().on_primary)
-        return super().enterEvent(event)
-
-    def leaveEvent(self, event: QEvent) -> None:
-        self.repaint(appearance.theme().on_surface)
-        return super().leaveEvent(event)
 
 
 class AIconCard(QPushButton):
