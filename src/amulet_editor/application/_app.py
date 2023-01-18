156bbdf1fb76be5c3e59da7c6d0842b42675bdfb
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-01-18 11:56:02 +0000

    Added theme back
    This should probably be moved to a plugin

--- src/amulet_editor/application/_app.py
+++ src/amulet_editor/application/_app.py
@@ -8,6 +8,7 @@ from PySide6.QtGui import QIcon
 from amulet_editor import __version__
 from amulet_editor.data import build
 import amulet_editor.data.plugin._manager as plugin_manager
+from . import appearance
 
 
 class AmuletApp(QApplication):
@@ -17,6 +18,8 @@ class AmuletApp(QApplication):
         self.setApplicationVersion(__version__)
         self.setWindowIcon(QIcon(build.get_resource("icons/amulet/Icon.ico")))
         self.setAttribute(Qt.AA_UseHighDpiPixmaps)
+
+        appearance.theme().apply(self)
 
         plugin_manager.init()
 
