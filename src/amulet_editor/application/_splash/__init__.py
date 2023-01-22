01684b20f60112030d2e7bf07b5c0a4d35edf356
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-01-22 14:33:11 +0000

    Added a simple splash screen

---
+++ src/amulet_editor/application/_splash/__init__.py
@@ -0,0 +1,18 @@
+from PySide6.QtCore import Qt, QCoreApplication
+from PySide6.QtGui import QPixmap
+
+from amulet_editor.data.build import get_resource
+
+from ._splash import Ui_Splash
+
+
+class Splash(Ui_Splash):
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self._logo.setPixmap(QPixmap(get_resource("icons/amulet/Icon.ico")))
+        self.setWindowTitle("Amulet Editor")
+
+    def showMessage(self, msg: str):
+        self._msg.setText(msg)
+        QCoreApplication.processEvents()
