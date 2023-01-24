6d4b9b2a5622dd1b7b3800b6c310417d8d8d6fcf
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-01-24 16:08:57 +0000

    Cleanup

--- src/amulet_editor/models/widgets/_traceback_dialog/__init__.py
+++ src/amulet_editor/models/widgets/_traceback_dialog/__init__.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Slot, Qt
-from PySide6.QtGui import QImage, QPixmap, QGuiApplication
+from PySide6.QtGui import QPixmap, QGuiApplication
 
 from ._traceback_dialog import Ui_TracebackDialog
 from amulet_editor.data.build import get_resource
@@ -17,7 +17,9 @@ class TracebackDialog(Ui_TracebackDialog):
         super().__init__(parent, f)
         self._traceback = traceback
         # alert_icon = QPixmap(QImage(get_resource("icons/tabler/alert-circle.svg"))).scaledToHeight(32)
-        alert_icon = QPixmap(get_resource("icons/tabler/alert-circle.svg")).scaledToHeight(32)
+        alert_icon = QPixmap(
+            get_resource("icons/tabler/alert-circle.svg")
+        ).scaledToHeight(32)
         self._alert_image.setPixmap(alert_icon)
         self._copy_button.clicked.connect(self._on_copy)
         self.setWindowTitle(title)
@@ -26,6 +28,5 @@ class TracebackDialog(Ui_TracebackDialog):
 
     @Slot()
     def _on_copy(self):
-        print("copying")
         clipboard = QGuiApplication.clipboard()
         clipboard.setText(self._traceback)
