dbef72c8bce1d0b8bda965fcbe68c21a4f97a141
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-03-13 16:39:08 +0000

    Added hover highlighting to the inspector (#39)

--- src/amulet_editor/plugins/amulet_team_inspector/_inspector/_inspector.py
+++ src/amulet_editor/plugins/amulet_team_inspector/_inspector/_inspector.py
@@ -29,9 +29,24 @@ class Ui_InspectionTool(QWidget):
         self.verticalLayout = QVBoxLayout(self)
         self.verticalLayout.setObjectName("verticalLayout")
 
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+
+        self.inspect_button = QPushButton(self)
+        self.inspect_button.setObjectName("inspect_button")
+        sizePolicy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(
+            self.inspect_button.sizePolicy().hasHeightForWidth()
+        )
+        self.inspect_button.setSizePolicy(sizePolicy)
+        self.horizontalLayout_2.addWidget(self.inspect_button)
+
         self.reload_button = QPushButton(self)
         self.reload_button.setObjectName("reload_button")
-        self.verticalLayout.addWidget(self.reload_button)
+        self.horizontalLayout_2.addWidget(self.reload_button)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
 
         self.tree_widget = QTreeWidget(self)
         self.tree_widget.setObjectName("tree_widget")
@@ -47,11 +62,11 @@ class Ui_InspectionTool(QWidget):
 
         self.run_button = QPushButton(self)
         self.run_button.setObjectName("run_button")
-        sizePolicy = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.run_button.sizePolicy().hasHeightForWidth())
-        self.run_button.setSizePolicy(sizePolicy)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.run_button.sizePolicy().hasHeightForWidth())
+        self.run_button.setSizePolicy(sizePolicy1)
         self.horizontalLayout.addWidget(self.run_button)
         self.verticalLayout.addLayout(self.horizontalLayout)
 
@@ -65,9 +80,8 @@ class Ui_InspectionTool(QWidget):
 
     def localise(self):
         self.setWindowTitle(QCoreApplication.translate("InspectionTool", "Form", None))
-        self.reload_button.setText(
-            QCoreApplication.translate("InspectionTool", "Reload", None)
-        )
+        self.inspect_button.setText("")
+        self.reload_button.setText("")
         self.run_button.setText(
             QCoreApplication.translate("InspectionTool", "Run", None)
         )
