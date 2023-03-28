5beb9fca3615b6fd9417aa7e352d30dcbd02b68e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-03-28 16:18:07 +0100

    Added a function to get the level

--- src/amulet_editor/data/project/_level.py
+++ src/amulet_editor/data/project/_level.py
@@ -6,3 +6,6 @@ from amulet_editor.models.minecraft import LevelData
 level_data: Optional[LevelData] = None
 
 level: Optional[BaseLevel] = None
+
+def get_level() -> Optional[BaseLevel]:
+    return level
