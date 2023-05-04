326b10ec50becf9f87804d4565076cf6b4bec216
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-05-04 11:29:44 +0100

    Reformatted

--- src/amulet_editor/__pyinstaller/hook-amulet_editor.py
+++ src/amulet_editor/__pyinstaller/hook-amulet_editor.py
@@ -1,6 +1,8 @@
 try:
     from PyInstaller.utils.hooks import collect_data_files
 except ImportError as e:
-    raise ImportError("This is part of the build pipeline. It cannot be imported at runtime.") from e
+    raise ImportError(
+        "This is part of the build pipeline. It cannot be imported at runtime."
+    ) from e
 
 datas = collect_data_files("amulet_editor")
