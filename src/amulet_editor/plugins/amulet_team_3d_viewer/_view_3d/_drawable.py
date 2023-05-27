666b92a33d106c987db06f7372336493723c43b1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-05-27 14:34:37 +0100

    Refactored drawable module

--- src/amulet_editor/plugins/amulet_team_3d_viewer/_view_3d/_drawable.py
+++ src/amulet_editor/plugins/amulet_team_3d_viewer/_view_3d/_drawable.py
@@ -5,39 +5,20 @@ from PySide6.QtGui import QMatrix4x4
 from PySide6.QtGui import QOpenGLContext
 
 
-class Drawable(ABC):
-    @abstractmethod
+class Drawable:
     def paintGL(self, projection_matrix: QMatrix4x4, view_matrix: QMatrix4x4):
         raise NotImplementedError
 
-
-class OpenGLDrawable:
-    __slots__ = ("__initialised", "__lock")
-
-    def __init__(self):
-        self.__initialised = False
-        self.__lock = Lock()
-
-    @abstractmethod
-    def _initializeGL(self):
-        """Initialise the OpenGL state."""
+    def initializeGL(self):
+        """Initialise the OpenGL data."""
         raise NotImplementedError
 
-    @abstractmethod
-    def _paintGL(self, projection_matrix: QMatrix4x4, view_matrix: QMatrix4x4):
-        """Paint the OpenGL data."""
+    def destroyGL(self):
+        """Destroy the OpenGL data."""
         raise NotImplementedError
 
-    def paintGL(self, projection_matrix: QMatrix4x4, view_matrix: QMatrix4x4):
-        if not self.__initialised:
-            with self.__lock:
-                if not self.__initialised:
-                    self._initializeGL()
-                    self.__initialised = True
-        self._paintGL(projection_matrix, view_matrix)
 
-
-class SharedOpenGLDrawable:
+class SharedOpenGLDrawable(ABC):
     """An OpenGL object shared between multiple contexts."""
 
     __slots__ = ("__shared_initialised", "__contexts", "__lock")
