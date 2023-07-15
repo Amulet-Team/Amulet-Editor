3e75e4e746b5991bad6c55cda68b41866a71c985
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-07-15 12:15:01 +0100

    Removed two methods from the View class
    These can be implemented with Qt events

--- src/amulet_editor/plugins/amulet_team_main_window/models/view.py
+++ src/amulet_editor/plugins/amulet_team_main_window/models/view.py
@@ -2,24 +2,7 @@ class View:
     def activate_view(self):
         """
         Run every time the view is activated by code or by clicking the tool button associated with the view.
-        :meth:`wake_view` will be called after if the view did not have focus.
         This is useful if the view wants to reset to a default state every time the tool button is clicked.
-        """
-        pass
-
-    def wake_view(self):
-        """
-        This is run when the view gains focus.
-        If the view was focused by clicking the associated tool button :meth:`activate_view` will be called first.
-        Useful in combination with :meth:`sleep_view` to switch between a high and low processing mode.
-        """
-        pass
-
-    def sleep_view(self):
-        """
-        This is run when the view looses focus.
-        This is useful to put the view into a low processing mode.
-        This method must leave the view in a state where it can be disabled or woken again.
         """
         pass
 
