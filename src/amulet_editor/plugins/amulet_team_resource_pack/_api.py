e3192a9472de7aa98f2ca4f634232eb1919fc5c9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-07-16 16:49:14 +0100

    Refactored the resource pack module into a class
    A class layout makes far more sense for this module because it allows all data, functions and signals to be combined into one object.

--- src/amulet_editor/plugins/amulet_team_resource_pack/_api.py
+++ src/amulet_editor/plugins/amulet_team_resource_pack/_api.py
@@ -1,12 +1,15 @@
-from weakref import WeakKeyDictionary
-from threading import Lock
+"""
+This module manages resource pack objects for each level
+"""
 
-from PySide6.QtCore import QObject, SignalInstance, QCoreApplication
+from weakref import WeakKeyDictionary
+from threading import Lock, RLock
+from typing import Optional
+
+from PySide6.QtCore import QObject, QCoreApplication, Signal
 
 from amulet.api.level import BaseLevel
-from amulet_editor.models.generic._singleton_signal import SingletonSignal
 from amulet_editor.models.generic._promise import Promise
-from amulet_editor.models.generic._key_lock import KeyLock
 
 from minecraft_model_reader import BaseResourcePackManager
 from minecraft_model_reader.api.resource_pack import (
@@ -18,86 +21,123 @@ from minecraft_model_reader.api.resource_pack.java.download_resources import (
 )
 
 
-# Each resource pack is unique to the level
-_level_locks = KeyLock()
-_resource_packs: WeakKeyDictionary[BaseLevel, BaseResourcePackManager] = WeakKeyDictionary()
+class ResourcePackContainer(QObject):
+    # Emitted with a promise object when a resource pack change is started.
+    changing = Signal(object)  # Promise[None]
+    # Emitted when the resource pack has changed.
+    changed = Signal()
 
-_signals_lock = Lock()
-_signals: WeakKeyDictionary[BaseLevel, tuple[QObject, SignalInstance]] = WeakKeyDictionary()
+    def __init__(self):
+        super().__init__()
+        self._lock = RLock()
+        self._resource_pack: Optional[BaseResourcePackManager] = None
+        self._loader: Optional[Promise[None]] = None
+
+    @property
+    def loader(self) -> Optional[Promise[None]]:
+        """
+        If the resource pack is being loaded this will be a promise.
+        This can be used to update a GUI to show the progress.
+        :return: A Promise instance if a resource pack is being loaded otherwise None.
+        """
+        return self._loader
+
+    @property
+    def loaded(self) -> bool:
+        """
+        Is there a valid resource pack.
+        :return: True if loaded otherwise False.
+        """
+        return self._resource_pack is not None
+
+    @property
+    def resource_pack(self) -> BaseResourcePackManager:
+        """
+        :return: The active resource pack for this level.
+        :raises RuntimeError: If the resource pack has not been initialised yet.
+        """
+        rp = self._resource_pack
+        if rp is None:
+            raise RuntimeError("The ResourcePackManager for this level has not been loaded yet.")
+        return rp
+
+    def init(self):
+        """
+        Initialise the default resource pack for this level if one does not already exist.
+        This is completed asynchronously. A promise is emitted from changing.
+
+        >>> def load(level: BaseLevel):
+        >>>     container = get_resource_pack_container(level)
+        >>>     promise = container.init(level)
+        >>>     def on_ready():
+        >>>         print("finished")
+        >>>         if promise.get_return():
+        >>>             print("resource pack was already loaded")
+        >>>         else:
+        >>>             print("resource pack has been loaded.")
+        >>>     promise.ready.connect(on_ready)
+        >>>     promise.progress_change.connect(lambda progress: print(progress))
+        >>>     promise.start()
+        """
+        def init(promise_data: Promise.Data) -> bool:
+            with self._lock:
+                if self._resource_pack is None:
+                    # TODO: support other resource pack formats
+                    promise_data.progress_text_change.emit(
+                        QCoreApplication.translate("ResourcePack", "downloading_resource_pack", None))
+                    try:
+                        it = get_java_vanilla_latest_iter()
+                        while True:
+                            progress = next(it)
+                            promise_data.progress_change.emit(progress * 0.5)
+                            if promise_data.is_cancel_requested():
+                                raise Promise.OperationCanceled()
+                    except StopIteration as e:
+                        vanilla = e.value
+
+                    self._resource_pack = load_resource_pack_manager(
+                        [
+                            vanilla,
+                            get_java_vanilla_fix()
+                        ],
+                        load=False
+                    )
+                    promise_data.progress_text_change.emit(
+                        QCoreApplication.translate("ResourcePack", "loading_resource_pack", None))
+                    for progress in self._resource_pack.reload():
+                        promise_data.progress_change.emit(0.5 + progress * 0.5)
+                    self.changed.emit()
+                    return False
+                return True
+
+        promise_ = Promise[bool](init)
+        old_loader = self._loader
+        if old_loader is not None:
+            old_loader.cancel()
+        self._loader = promise_
+        self.changing.emit(promise_)
+        promise_.start()
+
+    # def set_resource_pack(self, resource_pack: BaseResourcePackManager):
+    #     """
+    #     Set the resource pack.
+    #     Will emit a signal from changed after setting
+    #
+    #     :param resource_pack: The resource pack to set.
+    #     """
+    #     if not isinstance(resource_pack, BaseResourcePackManager):
+    #         raise TypeError("resource_pack must be an instance of BaseResourcePackManager")
+    #     with self._lock:
+    #         self._resource_pack = resource_pack
+    #         self.changed.emit()
 
 
-def resource_pack_changed(level: BaseLevel) -> SignalInstance:
-    """Get a Qt SignalInstance that will be emitted when the resource pack for that level changes."""
-    with _signals_lock:
-        if level not in _signals:
-            _signals[level] = SingletonSignal()
-        return _signals.get(level)[1]
+_lock = Lock()
+_level_data: WeakKeyDictionary[BaseLevel, ResourcePackContainer] = WeakKeyDictionary()
 
 
-def init_resource_pack(level: BaseLevel) -> Promise[None]:
-    """
-    An asynchronous loading function for the default resource pack.
-    The resource_pack_changed for the level will be emitted when loaded.
-    If the resource pack is already loaded this will do nothing.
-
-    >>> promise = init_resource_pack(level)
-    >>> promise.ready.connect(lambda: print("finished"))
-    >>> promise.progress_change.connect(lambda progress: print(progress))
-    >>> promise.start()
-    """
-    def init(promise_data: Promise.Data):
-        with _level_locks.get(level):
-            if _resource_packs.get(level) is None:
-                # TODO: support other resource pack formats
-                promise_data.progress_text_change.emit(QCoreApplication.translate("ResourcePack", "downloading_resource_pack", None))
-                try:
-                    it = get_java_vanilla_latest_iter()
-                    while True:
-                        progress = next(it)
-                        promise_data.progress_change.emit(progress)
-                        if promise_data.is_cancel_requested():
-                            raise Promise.OperationCanceled()
-                except StopIteration as e:
-                    vanilla = e.value
-
-                _resource_packs[level] = load_resource_pack_manager(
-                    [
-                        vanilla,
-                        get_java_vanilla_fix()
-                    ],
-                    load=False
-                )
-                resource_pack_changed(level).emit()
-
-    return Promise(init)
-
-
-def has_resource_pack(level: BaseLevel) -> bool:
-    return _resource_packs.get(level) is not None
-
-
-def get_resource_pack(level: BaseLevel) -> BaseResourcePackManager:
-    """
-    Get the resource pack for the level.
-    Will raise RuntimeError if the resource pack for the level has not been initialised.
-    You should use init_resource_pack before calling this for the first time to make sure one is initialised.
-    """
-    rp = _resource_packs.get(level)
-    if rp is None:
-        raise RuntimeError("The resource pack for this level has not been initialised yet.")
-    return rp
-
-
-def set_resource_pack(level: BaseLevel, resource_pack: BaseResourcePackManager):
-    """
-    Set the resource pack.
-    Will emit a signal from resource_pack_changed after setting
-
-    :param level: The level the resource pack is associated with.
-    :param resource_pack: The resource pack to set.
-    """
-    if not isinstance(resource_pack, BaseResourcePackManager):
-        raise TypeError("resource_pack must be an instance of BaseResourcePackManager")
-    with _level_locks.get(level):
-        _resource_packs[level] = resource_pack
-        resource_pack_changed(level).emit()
+def get_resource_pack_container(level: BaseLevel) -> ResourcePackContainer:
+    with _lock:
+        if level not in _level_data:
+            _level_data[level] = ResourcePackContainer()
+        return _level_data[level]
