31239d00fdd8db733d1baa8e5ee20ec7ebac67ba
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-07-20 14:56:44 +0100

    Refactored the resource pack class
    Merged the accessing functions into a class.
    Improved memory management.
    Converted the initialise generator into a Promise.
    Converted the OpenGL code to use Qt classes

--- src/amulet_editor/plugins/amulet_team_3d_viewer/_view_3d/_resource_pack.py
+++ src/amulet_editor/plugins/amulet_team_3d_viewer/_view_3d/_resource_pack.py
@@ -1,29 +1,19 @@
-from typing import Generator, Tuple, Dict, Optional
+from typing import Tuple, Dict, Optional, Iterable
 import struct
 import hashlib
 import os
 import json
-from PIL import Image
-import numpy
 import glob
 import logging
-from threading import Lock
-from weakref import WeakKeyDictionary
-from collections import deque
+from threading import Lock, RLock
+from weakref import WeakKeyDictionary, ref, WeakSet
 
-from PySide6.QtCore import QObject, SignalInstance
-from PySide6.QtGui import QOpenGLContext
-from OpenGL.GL import (
-    GL_TEXTURE_2D,
-    GL_RGBA,
-    GL_UNSIGNED_BYTE,
-    GL_TEXTURE_MIN_FILTER,
-    GL_NEAREST,
-    GL_CLAMP_TO_EDGE,
-    GL_TEXTURE_MAG_FILTER,
-    GL_TEXTURE_WRAP_S,
-    GL_TEXTURE_WRAP_T,
-)
+from PIL import Image
+from PIL.ImageQt import ImageQt
+
+from PySide6.QtCore import QObject, Signal
+from PySide6.QtGui import QImage, QOpenGLContext, QOffscreenSurface
+from PySide6.QtOpenGL import QOpenGLTexture
 
 from minecraft_model_reader.api.resource_pack.base import BaseResourcePackManager
 from minecraft_model_reader import BlockMesh
@@ -31,13 +21,13 @@ import PyMCTranslate
 from amulet.api.block import Block
 from amulet.api.level import BaseLevel
 
-from ._textureatlas import create_atlas_iter
+from ._textureatlas import create_atlas
 
+from amulet_editor.models.widgets.traceback_dialog import DisplayException
 from amulet_editor.models.generic._promise import Promise
-from amulet_editor.models.generic._singleton_signal import SingletonSignal
-from amulet_editor.models.generic._key_lock import KeyLock
+from amulet_editor.data.paths._application import cache_directory
 
-from amulet_team_resource_pack._api import get_resource_pack as _get_resource_pack, resource_pack_changed as _resource_pack_changed, init_resource_pack as _init_resource_pack
+from amulet_team_resource_pack._api import get_resource_pack_container
 
 log = logging.getLogger(__name__)
 
@@ -49,6 +39,7 @@ class OpenGLResourcePack:
     """
 
     _lock = Lock()
+    _resource_pack: BaseResourcePackManager
     # The translator to look up the version block
     _translator: PyMCTranslate.Version
     # Loaded block models
@@ -56,13 +47,14 @@ class OpenGLResourcePack:
     # Texture coordinates
     _texture_bounds: Dict[str, Tuple[float, float, float, float]]
 
+    # Image data
+    _atlas: Optional[QImage]
     # Image on CPU
-    _image_width: int
-    _image_height: int
-    _image: Optional[numpy.ndarray]
-
-    # Image on GPU
-    _gl_textures: WeakKeyDictionary[QOpenGLContext, int]
+    _texture: Optional[QOpenGLTexture]
+    # All the contexts the texture has been bound to.
+    # We need to destroy the GPU data for the texture in each context when this class is destroyed
+    # because the context may exist for a long time after.
+    _contexts: WeakSet[QOpenGLContext]
 
     def __init__(
         self, resource_pack: BaseResourcePackManager, translator: PyMCTranslate.Version
@@ -73,105 +65,112 @@ class OpenGLResourcePack:
         self._translator = translator
         self._block_models = {}
         self._texture_bounds = {}
-        self._image = None
-        self._image_width = 0
-        self._image_height = 0
-        self._gl_textures = WeakKeyDictionary()
+        self._atlas = None
+        self._texture = None
+        self._contexts = WeakSet()
 
-    def initialise(self) -> Generator[float, None, None]:
-        """Create the atlas texture."""
-        with self._lock:
-            if self._image is None:
-                cache_id = struct.unpack(
-                    "H",
-                    hashlib.sha1(
-                        "".join(self._resource_pack.pack_paths).encode("utf-8")
-                    ).digest()[:2],
-                )[0]
+    def __del__(self):
+        self._destroy_gl(*self._contexts)
 
-                atlas: Image.Image
+    def initialise(self) -> Promise[None]:
+        """
+        Create the atlas texture.
+        This is run asynchronously.
+        """
+        def func(promise_data: Promise.Data):
+            with self._lock, DisplayException("Error initialising the resource pack."):
+                if self._texture is None:
+                    cache_id = struct.unpack(
+                        "H",
+                        hashlib.sha1(
+                            "".join(self._resource_pack.pack_paths).encode("utf-8")
+                        ).digest()[:2],
+                    )[0]
 
-                if not self._resource_pack.pack_paths:
-                    log.warning("There are no resource packs to load.")
+                    atlas: Image.Image
 
-                mod_time = max(
-                    (
-                        os.stat(path).st_mtime
-                        for pack in self._resource_pack.pack_paths
-                        for path in glob.glob(
-                            os.path.join(glob.escape(pack), "**", "*.*"), recursive=True
-                        )
-                    ),
-                    default=0,
-                )
+                    if not self._resource_pack.pack_paths:
+                        log.warning("There are no resource packs to load.")
 
-                cache_dir = os.path.join(".", "cache", "resource_pack")
-                img_path = os.path.join(cache_dir, f"{cache_id}.png")
-                bounds_path = os.path.join(cache_dir, f"{cache_id}.json")
-                try:
-                    with open(bounds_path) as f:
-                        cache_mod_time, bounds = json.load(f)
-                    if mod_time != cache_mod_time:
-                        raise Exception(
-                            "The resource packs have changed since last merging."
-                        )
-                    atlas = Image.open(img_path)
-                except:
-                    atlas_iter = create_atlas_iter(
-                        self._resource_pack.textures
+                    mod_time = max(
+                        (
+                            os.stat(path).st_mtime
+                            for pack in self._resource_pack.pack_paths
+                            for path in glob.glob(
+                                os.path.join(glob.escape(pack), "**", "*.*"), recursive=True
+                            )
+                        ),
+                        default=0,
                     )
+
+                    cache_dir = os.path.join(cache_directory(), "resource_pack")
+                    img_path = os.path.join(cache_dir, f"{cache_id}.png")
+                    bounds_path = os.path.join(cache_dir, f"{cache_id}.json")
                     try:
-                        while True:
-                            yield next(atlas_iter)
-                    except StopIteration as e:
+                        with open(bounds_path) as f:
+                            cache_mod_time, bounds = json.load(f)
+                        if mod_time != cache_mod_time:
+                            raise Exception(
+                                "The resource packs have changed since last merging."
+                            )
+                        self._atlas = QImage(img_path)
+                    except Exception:
                         (
                             atlas,
                             bounds,
-                        ) = e.value
+                        ) = create_atlas(self._resource_pack.textures).call_chained(promise_data)
+
                         os.makedirs(cache_dir, exist_ok=True)
                         atlas.save(img_path)
                         with open(bounds_path, "w") as f:
                             json.dump((mod_time, bounds), f)
+                        self._atlas = ImageQt(atlas).mirrored()
 
-                self._image_width, self._image_height = atlas.size
-                self._image = numpy.array(atlas, numpy.uint8).ravel()
-                self._texture_bounds = bounds
+                    self._texture_bounds = bounds
 
-    def get_atlas_id(self) -> int:
-        """Get the opengl texture id of the atlas in the current context."""
-        context = QOpenGLContext.currentContext()
-        if context is None:
-            raise RuntimeError("Context must be set before calling this.")
-        if context not in self._gl_textures:
-            # Ensure the atlas is initialised.
-            # This will do nothing if it has already been initialised and will block if it is being initialised.
-            deque(self.initialise(), maxlen=0)
+        return Promise(func)
 
-            f = context.functions()
-            # Create the texture location
-            gl_texture = self._gl_textures[context] = f.glGenTextures(1)
-            f.glBindTexture(GL_TEXTURE_2D, gl_texture)
-            f.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-            f.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-            f.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
-            f.glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
+    def get_texture(self) -> QOpenGLTexture:
+        """
+        Get the opengl texture for the atlas.
+        The GPU data will be destroyed when the context is about to be destroyed
+        or when the last reference to this instance is released.
+        :return: A QOpenGLTexture instance. This must not be stored outside of this class.
+        """
+        with self._lock:
+            context = QOpenGLContext.currentContext()
+            if context is None:
+                raise RuntimeError("No context has been made current")
+            if self._texture is None:
+                if self._atlas is None:
+                    raise RuntimeError("The OpenGLResourcePack has not been initialised.")
+                self._texture = QOpenGLTexture(QOpenGLTexture.Target.Target2D)
+                self._texture.setMinificationFilter(QOpenGLTexture.Filter.Nearest)
+                self._texture.setMagnificationFilter(QOpenGLTexture.Filter.Nearest)
+                self._texture.setWrapMode(QOpenGLTexture.CoordinateDirection.DirectionS,
+                                          QOpenGLTexture.WrapMode.ClampToEdge)
+                self._texture.setWrapMode(QOpenGLTexture.CoordinateDirection.DirectionT,
+                                          QOpenGLTexture.WrapMode.ClampToEdge)
+                self._texture.setData(self._atlas)
+                # The atlas CPU texture is no longer needed.
+                self._atlas = None
+            if not self._texture.isCreated():
+                self._texture.create()
+                context.aboutToBeDestroyed.connect(lambda: self._destroy_gl(context))
+                self._contexts.add(context)
+            return self._texture
 
-            f.glBindTexture(GL_TEXTURE_2D, gl_texture)
-            f.glTexImage2D(
-                GL_TEXTURE_2D,
-                0,
-                GL_RGBA,
-                self._image_width,
-                self._image_height,
-                0,
-                GL_RGBA,
-                GL_UNSIGNED_BYTE,
-                self._image,
-            )
-            f.glBindTexture(GL_TEXTURE_2D, 0)
-            log.debug(f"Finished setting up texture atlas in OpenGL context {context}")
-
-        return self._gl_textures[context]
+    def _destroy_gl(self, *contexts: QOpenGLContext):
+        """Destroy the GPU data in the given contexts."""
+        with self._lock:
+            if self._texture is not None:
+                surface = QOffscreenSurface()
+                for context in contexts:
+                    log.debug(f"Destroying resource pack GPU image for context {context}")
+                    context.makeCurrent(surface)
+                    if self._texture.isCreated():
+                        self._texture.destroy()
+                    context.doneCurrent()
 
     def get_texture_path(self, namespace: Optional[str], relative_path: str):
         """Get the absolute path of the image from the relative components.
@@ -205,73 +204,74 @@ class OpenGLResourcePack:
         return self._block_models[universal_block]
 
 
-# Each resource pack is unique to the level
-_level_locks = KeyLock()
-_resource_packs: WeakKeyDictionary[BaseLevel, OpenGLResourcePack] = WeakKeyDictionary()
+class RenderResourcePackContainer(QObject):
+    # Emitted with a promise object when a resource pack change is started.
+    changing = Signal(object)  # Promise[None]
+    # Emitted when the resource pack has changed.
+    changed = Signal()
+
+    def __init__(self, level: BaseLevel):
+        super().__init__()
+        self._level = ref(level)
+        self._lock = RLock()
+        self._resource_pack: Optional[OpenGLResourcePack] = None
+        self._loader: Optional[Promise[None]] = None
+        self._resource_pack_container = get_resource_pack_container(level)
+        self._resource_pack_container.changed.connect(self._reload)
+
+    @property
+    def loader(self) -> Optional[Promise[None]]:
+        return self._loader
+
+    @property
+    def loaded(self) -> bool:
+        """Is there a valid resource pack."""
+        return self._resource_pack is not None
+
+    @property
+    def resource_pack(self) -> OpenGLResourcePack:
+        rp = self._resource_pack
+        if rp is None:
+            raise RuntimeError("The OpenGLResourcePack for this level has not been loaded yet.")
+        return rp
+
+    def _reload(self):
+        def func(promise_data: Promise.Data):
+            level = self._level()
+            with self._lock:
+                resource_pack = self._resource_pack_container.resource_pack
+                # TODO: modify the resource pack library to expose the desired translator
+                translator = level.translation_manager.get_version("java", (999, 0, 0))
+
+                rp = OpenGLResourcePack(resource_pack, translator)
+                promise = rp.initialise()
+                # TODO: support canceling
+                promise.call_chained(promise_data)
+                # for progress in rp.initialise():
+                #     if promise_data.is_cancel_requested or _tokens.get(level) is not token:
+                #         # Abort if a new generation has been started
+                #         raise Promise.OperationCanceled()
+                #
+                #     promise_data.progress_change.emit(0.5 + progress * 0.5)
+
+                self._resource_pack = rp
+                self.changed.emit()
+
+        promise_ = Promise(func)
+        old_loader = self._loader
+        if old_loader is not None:
+            old_loader.cancel()
+        self._loader = promise_
+        self.changing.emit(promise_)
+        promise_.start()
+
 
 _lock = Lock()
-_signals: WeakKeyDictionary[BaseLevel, tuple[QObject, SignalInstance]] = WeakKeyDictionary()
-_tokens: WeakKeyDictionary[BaseLevel, Optional[object]] = WeakKeyDictionary()
+_level_data: WeakKeyDictionary[BaseLevel, RenderResourcePackContainer] = WeakKeyDictionary()
 
 
-def render_resource_pack_changed(level: BaseLevel) -> SignalInstance:
-    """Get a Qt SignalInstance that will be emitted when the resource pack for that level changes."""
+def get_gl_resource_pack_container(level: BaseLevel) -> RenderResourcePackContainer:
     with _lock:
-        if level not in _signals:
-            _signals[level] = SingletonSignal()
-        return _signals.get(level)[1]
-
-
-def load_render_resource_pack(level: BaseLevel) -> Promise[None]:
-    """
-    Initialise the render resource pack for the level
-
-    :param level: The level to load the render resource pack for.
-    :return: None. render_resource_pack_changed for the level will be emitted when finished.
-    """
-
-    token = object()
-
-    with _lock:
-        if level not in _tokens:
-            _resource_pack_changed(level).connect(lambda: load_render_resource_pack(level))
-        _tokens[level] = token
-
-    def func(promise_data: Promise.Data):
-        promise = _init_resource_pack(level)
-        promise.call_chained(promise_data, 0.0, 0.5)
-
-        resource_pack = _get_resource_pack(level)
-        # TODO: modify the resource pack library to expose the desired translator
-        translator = level.translation_manager.get_version("java", (999, 0, 0))
-
-        rp = OpenGLResourcePack(resource_pack, translator)
-        for progress in rp.initialise():
-            if promise_data.is_cancel_requested:
-                raise Promise.OperationCanceled()
-
-            if _tokens.get(level) is not token:
-                # Abort if a new generation has been started
-                return
-
-            promise_data.progress_change.emit(0.5 + progress * 0.5)
-
-        _resource_packs[level] = rp
-        render_resource_pack_changed(level).emit()
-
-    return Promise(func)
-
-
-def has_render_resource_pack(level: BaseLevel) -> bool:
-    return _resource_packs.get(level) is not None
-
-
-def get_render_resource_pack(level: BaseLevel) -> OpenGLResourcePack:
-    """
-    Get the resource pack for the level.
-    Will raise a runtime error if this is called before it is initialised.
-    """
-    rp = _resource_packs.get(level)
-    if rp is None:
-        raise RuntimeError("The OpenGLResourcePack for this level has not been loaded yet.")
-    return rp
+        if level not in _level_data:
+            _level_data[level] = RenderResourcePackContainer(level)
+        return _level_data[level]
