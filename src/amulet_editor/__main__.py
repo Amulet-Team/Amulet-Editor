99222d2b75811829a13e73bd90bb2dadd7e13261
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-08-15 11:00:11 +0100

    Refactored main script
    The main script is now a bit more defensive.
    It tries to log the errors via the logging module.
    If that fails it manually prints to the console and saves to a file.

--- src/amulet_editor/__main__.py
+++ src/amulet_editor/__main__.py
@@ -5,80 +5,143 @@ def main():
     try:
         # Verify the python version
         import sys
+
         if sys.version_info[:2] < (3, 9):
             raise Exception("Must be using Python 3.9+")
 
         # This is required when running from a frozen bundle (eg pyinstaller)
         from multiprocessing import freeze_support
+
         freeze_support()
 
         # Initialise default paths
         from amulet_editor.data.paths._application import _init_paths
+
         _init_paths(None, None, None, None)
 
         from amulet_editor.application._console import init_console
+
         init_console()
 
         # Enable useful tracebacks for access violations and other hard crashes
         import faulthandler
+
         faulthandler.enable()
 
+        # Import and boot the app.
         from amulet_editor.application._main import app_main
 
+        app_main()
+
     except Exception as e:
+        # Something crashed
+
+        # Log the output to a file
         try:
-            import traceback
+            # If the logging module has been initialised then log to that
+            import logging
             import sys
+            import os
+            from datetime import datetime
 
-        except ImportError as e:
-            # Something has gone seriously wrong
-            print(e)
-            print("Failed to import requirements. Check that you extracted correctly.")
-            input("Press ENTER to continue.")
-        else:
-            err = "\n".join(
-                [traceback.format_exc()]
-                + ["Failed to import requirements. Check that you extracted correctly."]
-                * isinstance(e, ImportError)
-                + [str(e)]
-            )
-            print(err)
-            try:
-                with open("crash.log", "w") as f:
-                    f.write(err)
-            except OSError:
-                pass
-            input("Press ENTER to continue.")
-            sys.exit(1)
+            if not any(
+                isinstance(h, logging.FileHandler) for h in logging.getLogger().handlers
+            ):
+                # Set up a file handler if one does not exist
 
-    else:
-        # Everything imported correctly. Boot the app.
-        try:
-            app_main()
-        except Exception as e:
+                try:
+                    from amulet_editor.data.paths._application import logging_directory
+
+                    log_dir = logging_directory()
+                except Exception:
+                    log_dir = "."
+
+                file_path = os.path.join(
+                    log_dir,
+                    f"amulet-log-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}-{os.getpid()}.txt",
+                )
+                file_handler = logging.FileHandler(file_path)
+
+                logging.basicConfig(
+                    level=logging.WARNING,
+                    format="%(levelname)s - %(threadName)s - %(name)s - %(message)s",
+                    force=True,
+                    handlers=[logging.StreamHandler(sys.__stderr__), file_handler],
+                )
+
             logging.exception(e)
             logging.error(
-                f"Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue."
+                "Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue."
             )
+
+        except Exception:
+            # If using the logging module failed then try just writing it.
             try:
                 import traceback
-                from PySide6.QtWidgets import QApplication
-                from amulet_editor.models.widgets.traceback_dialog import (
-                    display_exception_blocking,
-                )
 
-                if QApplication.instance() is None:
-                    # QDialog needs an app otherwise it crashes
-                    app = QApplication()
-                display_exception_blocking(
-                    title="Error Initialising Application",
-                    error=str(e),
-                    traceback="".join(traceback.format_exc()),
+                tb = traceback.format_exc()
+            except Exception:
+                tb = None
+
+            try:
+                print(e)
+                if tb is not None:
+                    print(tb)
+                print(
+                    "Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue."
                 )
-            except:
+                import os
+
+                try:
+                    from amulet_editor.data.paths._application import logging_directory
+
+                    log_dir = logging_directory()
+                except Exception:
+                    log_dir = "."
+                with open(os.path.join(log_dir, f"crash_{os.getpid()}.log"), "w") as f:
+                    if tb is not None:
+                        f.write(tb)
+                    f.write(str(e))
+                    f.write(
+                        "Amulet Crashed. Sorry about that. Please report it to a developer if you think this is an issue."
+                    )
+            except Exception:
                 pass
-            input("Press ENTER to continue...")
-            raise e
+
+        try:
+            # Try reporting the crash with a GUI
+            import traceback
+            from PySide6.QtWidgets import QApplication
+            from amulet_editor.models.widgets.traceback_dialog import (
+                display_exception_blocking,
+            )
+
+            if QApplication.instance() is None:
+                # QDialog needs an app otherwise it crashes
+                app = QApplication()
+            display_exception_blocking(
+                title="Error Initialising Application",
+                error=str(e),
+                traceback="".join(traceback.format_exc()),
+            )
+        except Exception:
+            pass
+
+        try:
+            # If a console is being shown then pause to let the user read it.
+            import sys
+
+            if sys.stdin is not None:
+                input("Press ENTER to continue.")
+        except Exception:
+            pass
+
+        try:
+            import sys
+
+            sys.exit(1)
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
