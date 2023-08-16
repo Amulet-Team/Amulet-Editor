2bcb8e909809174540c6a76d75b3ec7a6a059670
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-08-16 11:33:50 +0100

    Added missing files to the pyinstaller hook

--- src/amulet_editor/__pyinstaller/hook-amulet_editor.py
+++ src/amulet_editor/__pyinstaller/hook-amulet_editor.py
@@ -1,8 +1,26 @@
 try:
-    from PyInstaller.utils.hooks import collect_data_files
+    from PyInstaller.utils.hooks import (
+        collect_data_files,
+        copy_metadata,
+        collect_submodules,
+    )
 except ImportError as e:
     raise ImportError(
         "This is part of the build pipeline. It cannot be imported at runtime."
     ) from e
 
-datas = collect_data_files("amulet_editor")
+datas = [
+    *collect_data_files("PySide6", includes=["*.pyi", "py.typed"]),
+    *collect_data_files("amulet_editor", excludes=["**/*.ui", "**/*.c", "**/*.pyx"]),
+    *collect_data_files(
+        "amulet_editor.plugins",
+        include_py_files=True,
+        excludes=["**/*.ui", "**/*.c", "**/*.pyx", "**/*.pyc"],
+    ),
+    *copy_metadata("amulet_editor", recursive=True),
+]
+
+hiddenimports = [
+    *collect_submodules("amulet_editor", lambda name: name != "amulet_editor.plugins"),
+    *collect_submodules("PySide6"),
+]
