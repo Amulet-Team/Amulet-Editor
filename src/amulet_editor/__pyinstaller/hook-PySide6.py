fa87d18c2408d4abdbf9faa209b0cc085b3ff070
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-08-31 14:51:32 +0100

    Fix linux installer (#58)
    * Split up pyinstaller hooks
    
    Add linux shared object libraries
    
    * Added debug output
    
    * Fixed glob
    
    * Removed print statements
    
    * print amulet_editor hidden imports
    
    For some reason on linux a singular module is not being included
    
    * Added debug output in build process
    
    * Added libegl to ubuntu
    
    * Reformatted

---
+++ src/amulet_editor/__pyinstaller/hook-PySide6.py
@@ -0,0 +1,17 @@
+import glob
+import os
+from PyInstaller.utils.hooks import (
+    collect_data_files,
+    collect_submodules,
+)
+
+import PySide6
+
+hiddenimports = collect_submodules("PySide6")
+binaries = [
+    (path, ".")
+    for path in glob.glob(
+        os.path.join(glob.escape(PySide6.__path__[0]), "Qt", "lib", "*.so*")
+    )
+]
+datas = collect_data_files("PySide6", includes=["*.pyi", "py.typed"])
--- src/amulet_editor/__pyinstaller/hook-amulet_editor.py
+++ src/amulet_editor/__pyinstaller/hook-amulet_editor.py
@@ -1,16 +1,10 @@
-try:
-    from PyInstaller.utils.hooks import (
-        collect_data_files,
-        copy_metadata,
-        collect_submodules,
-    )
-except ImportError as e:
-    raise ImportError(
-        "This is part of the build pipeline. It cannot be imported at runtime."
-    ) from e
+from PyInstaller.utils.hooks import (
+    collect_data_files,
+    copy_metadata,
+    collect_submodules,
+)
 
 datas = [
-    *collect_data_files("PySide6", includes=["*.pyi", "py.typed"]),
     *collect_data_files("amulet_editor", excludes=["**/*.ui", "**/*.c", "**/*.pyx"]),
     *collect_data_files(
         "amulet_editor.plugins",
@@ -22,6 +16,6 @@ datas = [
 
 hiddenimports = [
     *collect_submodules("amulet_editor", lambda name: name != "amulet_editor.plugins"),
-    *collect_submodules("PySide6"),
-    *collect_submodules("OpenGL"),  # On my linux install not everything was included.
+    "PySide6",
+    "OpenGL",
 ]
