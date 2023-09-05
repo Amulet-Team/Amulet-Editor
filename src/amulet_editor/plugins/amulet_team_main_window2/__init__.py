4bd4ef5f4dc22f0009f35cf9d44e7399ce599cca
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-09-05 11:14:09 +0100

    Exposed some attributes

--- src/amulet_editor/plugins/amulet_team_main_window2/__init__.py
+++ src/amulet_editor/plugins/amulet_team_main_window2/__init__.py
@@ -1,1 +1,12 @@
-# from ._plugin import plugin
+from ._plugin import plugin  # Private plugin initialisation
+
+# Public functions and classes
+from ._application.widget import Widget, register_widget, unregister_widget
+from ._application.windows.window_proxy import AbstractWindowProxy
+from ._application.windows.main_window import (
+    AmuletMainWindowProxy,
+    get_main_window,
+    add_toolbar_button,
+    add_static_toolbar_button,
+    remove_toolbar_button,
+)
