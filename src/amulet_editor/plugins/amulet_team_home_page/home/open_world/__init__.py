4990dac60412e72d50ab3d9ee3ec80e52173583d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-09-05 13:48:26 +0100

    Added a tablericons plugin (#63)
    * Added a tablericons plugin
    
    I find it more useful to refer to the icon's path by a variable rather than having to resolve the path each time I want to use it.
    This module stores all the icons as well as variables to the paths
    
    * Reformatted

--- src/amulet_editor/plugins/amulet_team_home_page/home/open_world/__init__.py
+++ src/amulet_editor/plugins/amulet_team_home_page/home/open_world/__init__.py
@@ -1,15 +1,15 @@
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QFileDialog, QApplication
-from amulet_editor.data.build import get_resource
 from ._open_world import Ui_OpenWorldPage
 from amulet_editor.application._cli import spawn_process
 from amulet_editor.data.project import get_level
+import tablericons
 
 
 class OpenWorldPage(Ui_OpenWorldPage):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.btn_back.setIcon(QIcon(get_resource("icons/tabler/arrow-left.svg")))
+        self.btn_back.setIcon(QIcon(tablericons.arrow_left))
         self.load_file_button.clicked.connect(self.open_file)
         self.load_directory_button.clicked.connect(self.open_dir)
 
