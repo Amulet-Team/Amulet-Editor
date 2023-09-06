70a7b86340fa5967ae96fd4b66810c012d485672
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-09-06 10:12:20 +0100

    Added the 3D view to the tab engine

--- src/amulet_editor/plugins/amulet_team_3d_viewer/_view_3d/_view.py
+++ src/amulet_editor/plugins/amulet_team_3d_viewer/_view_3d/_view.py
@@ -6,12 +6,14 @@ import traceback
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
 
-from amulet_team_main_window.application.windows.main_window import View
+from amulet_team_main_window2 import Widget
 
 from ._renderer import FirstPersonCanvas
 
 
-class View3D(QWidget, View):
+class View3D(QWidget, Widget):
+    name = "3D View"
+
     def __init__(
         self, parent: Optional[QWidget] = None, f: Qt.WindowType = Qt.WindowType.Widget
     ):
