54d0a10b497bdca5b23cf32e27078ab14dd5556d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-09-06 13:43:07 +0100

    Added a thread_manager plugin
    If a thread is not stopped before the application exits it will crash the program.
    Calling quit and then wait for each thread is the general suggestion but this means the program needs to wait for each thread to finish before quitting the next thread.
    This plugin automatically quits all threads and then waits for all threads which means that all the threads can finish in parallel.

---
+++ src/amulet_editor/plugins/thread_manager/_plugin.py
@@ -0,0 +1,44 @@
+import logging
+
+from shiboken6 import isValid
+from PySide6.QtCore import QThread
+
+from amulet_editor.models.plugin import PluginV1
+
+log = logging.getLogger(__name__)
+
+_threads: set[tuple[str, QThread]] = set()
+
+
+def new_thread(name: str) -> QThread:
+    """
+    Get a new QThread instance.
+    When you are done with the thread you should quit and delete it.
+    This plugin will handle waiting for the thread to finish so that the application does not crash.
+    """
+    thread = QThread()
+    thread.setObjectName(name)
+    storage = name, thread
+    _threads.add(storage)
+    thread.finished.connect(lambda: _threads.remove(storage))
+    return thread
+
+
+def unload_plugin():
+    threads = tuple(_threads)
+
+    # Quit all threads.
+    # These will run concurrently.
+    for thread_name, thread in threads:
+        if isValid(thread):
+            log.debug(f"Quitting thread {thread_name}. {thread}")
+            thread.quit()
+
+    # Wait for all the threads to finish
+    for thread_name, thread in threads:
+        if isValid(thread):
+            log.debug(f"Waiting for thread {thread_name}. {thread}")
+            thread.wait()
+
+
+plugin = PluginV1(unload=unload_plugin)
