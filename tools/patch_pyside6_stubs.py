f131a1ae89bfe9bcc78ea883e86c5be829ea7ae6
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-05-07 16:10:54 +0100

    Replaced gen pyside6 stubs with patch script
    This new script patches some issues in the stub files distributed with PySide6.

---
+++ tools/patch_pyside6_stubs.py
@@ -0,0 +1,33 @@
+import os
+import PySide6
+
+PySide6Path = PySide6.__path__[0]
+
+Patches: dict[str, list[tuple[str, str]]] = {
+    os.path.join(PySide6Path, "QtCore.pyi"): [
+        (
+            "def translate(self, context: Union[bytes, bytearray, memoryview], sourceText: Union[bytes, bytearray, memoryview], disambiguation: Union[bytes, bytearray, memoryview, NoneType] = ..., n: int = ...) -> str: ...",
+            "def translate(self, context: Union[str, bytes, bytearray, memoryview], sourceText: Union[str, bytes, bytearray, memoryview], disambiguation: Union[str, bytes, bytearray, memoryview, NoneType] = ..., n: int = ...) -> str: ...",
+        )
+    ],
+    os.path.join(PySide6Path, "QtGui.pyi"): (
+        (
+            "def setParent(self, parent: PySide6.QtGui.QWindow",
+            "def setParent(self, parent: Optional[PySide6.QtGui.QWindow]",
+        ),
+    )
+}
+
+
+def main() -> None:
+    for stub_path, patches in Patches.items():
+        with open(stub_path) as f:
+            stub = f.read()
+        for find, replace in patches:
+            stub = stub.replace(find, replace)
+        with open(stub_path, "w") as f:
+            f.write(stub)
+
+
+if __name__ == '__main__':
+    main()
