eee659ff5cd3aa13f780e1cad22cc5f94c16b9e4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-05-16 11:46:08 +0100

    Disable layout

--- src/builtin_plugins/amulet_team_layout/_plugin.py
+++ src/builtin_plugins/amulet_team_layout/_plugin.py
@@ -4,24 +4,26 @@ from amulet_editor.data.level import get_level
 from amulet_editor.models.plugin import PluginV1
 
 
-from amulet_team_main_window.api import (
-    register_view,
-    unregister_view,
-    get_active_window,
-)
+# from amulet_team_main_window.api import (
+#     register_view,
+#     unregister_view,
+#     get_active_window,
+# )
 import tablericons
 
-from amulet_team_3d_viewer._view_3d import View3D
+# from amulet_team_3d_viewer._view_3d import View3D
 
 
 def load_plugin() -> None:
-    if get_level() is not None:
-        register_view(View3D, tablericons.three_d_cube_sphere, "3D Editor")
-        get_active_window().activate_view(View3D)
+    pass
+    # if get_level() is not None:
+    #     register_view(View3D, tablericons.three_d_cube_sphere, "3D Editor")
+    #     get_active_window().activate_view(View3D)
 
 
 def unload_plugin() -> None:
-    unregister_view(View3D)
+    pass
+    # unregister_view(View3D)
 
 
 plugin = PluginV1(load_plugin, unload_plugin)
