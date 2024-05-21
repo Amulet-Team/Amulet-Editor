965429fbefe9afe70b123df0e69e72d8b551990a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-05-21 16:47:21 +0100

    Add home layout and button

--- src/builtin_plugins/amulet_team_default_layout/_plugin.py
+++ src/builtin_plugins/amulet_team_default_layout/_plugin.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from amulet_editor.data.level import get_level
 from amulet_editor.models.plugin import PluginV1
-from amulet_team_main_window import ButtonProxy, create_layout_button
+from amulet_team_main_window import register_layout, WidgetConfig, WindowConfig, LayoutConfig, ButtonProxy, create_layout_button
+from amulet_team_home_page import HomeWidget
 
 
 # from amulet_team_main_window.api import (
@@ -14,31 +15,33 @@ import tablericons
 
 # from amulet_team_3d_viewer._view_3d import View3D
 
+HomeLayoutID = "073bfd20-249e-4e0c-ad41-0bcb0c9db89f"
 home_button: ButtonProxy | None = None
 select_button: ButtonProxy | None = None
-
-
-def _set_home_layout() -> None:
-    pass
-    # amulet_team_main_window.get_main_window().set_layout(HomeWidget)
 
 
 def load_plugin() -> None:
     global home_button, select_button
 
-    # Set up the home button
-    # home_button = create_layout_button()
-    # home_button.set_icon(tablericons.home)
-    # home_button.set_name("Home")
-    # home_button.set_callback(_set_home_layout)
+    register_layout(HomeLayoutID, LayoutConfig(
+        WindowConfig(
+            None, None, WidgetConfig(HomeWidget.__qualname__)
+        ),
+        (),
+    ))
 
-    # if get_level() is None:
-    #     # Make the home layout active by clicking the button
-    #     home_button.click()
-    # else:
-    #     pass
-    #     # register_view(View3D, tablericons.three_d_cube_sphere, "3D Editor")
-    #     # get_active_window().activate_view(View3D)
+    # Set up the home button
+    home_button = create_layout_button(HomeLayoutID)
+    home_button.set_icon(tablericons.home)
+    home_button.set_name("Home")
+
+    if get_level() is None:
+        # Make the home layout active by clicking the button
+        home_button.click()
+    else:
+        pass
+        # register_view(View3D, tablericons.three_d_cube_sphere, "3D Editor")
+        # get_active_window().activate_view(View3D)
 
 
 def unload_plugin() -> None:
