171ed9ea2b3ae91087eef15666dfaee974f14e87
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-05-23 11:02:52 +0100

    Refactored the layout plugin

--- src/builtin_plugins/amulet_team_default_layout/_plugin.py
+++ src/builtin_plugins/amulet_team_default_layout/_plugin.py
@@ -2,30 +2,27 @@ from __future__ import annotations
 
 from amulet_editor.data.level import get_level
 from amulet_editor.models.plugin import PluginV1
-from amulet_team_main_window import register_layout, WidgetConfig, WindowConfig, LayoutConfig, ButtonProxy, create_layout_button
+from amulet_team_main_window import register_layout, unregister_layout, WidgetConfig, WidgetStackConfig, WindowConfig, LayoutConfig, ButtonProxy, create_layout_button
 from amulet_team_home_page import HomeWidget
 
-
-# from amulet_team_main_window.api import (
-#     register_view,
-#     unregister_view,
-#     get_active_window,
-# )
 import tablericons
-
-# from amulet_team_3d_viewer._view_3d import View3D
 
 HomeLayoutID = "073bfd20-249e-4e0c-ad41-0bcb0c9db89f"
 home_button: ButtonProxy | None = None
-select_button: ButtonProxy | None = None
 
 
 def load_plugin() -> None:
-    global home_button, select_button
+    global home_button
 
     register_layout(HomeLayoutID, LayoutConfig(
         WindowConfig(
-            None, None, WidgetConfig(HomeWidget.__qualname__)
+            None,
+            None,
+            WidgetStackConfig(
+                (
+                    WidgetConfig(HomeWidget.__qualname__),
+                )
+            )
         ),
         (),
     ))
@@ -45,8 +42,9 @@ def load_plugin() -> None:
 
 
 def unload_plugin() -> None:
-    pass
-    # unregister_view(View3D)
+    if home_button is not None:
+        home_button.delete()
+    unregister_layout(HomeLayoutID)
 
 
 plugin = PluginV1(load=load_plugin, unload=unload_plugin)
