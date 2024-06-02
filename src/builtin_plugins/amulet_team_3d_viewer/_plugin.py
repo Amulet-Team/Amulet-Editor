e64dd372189a6a1a00651572ed47b321b1653d8c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-02 08:43:12 +0100

    Refactor View3D plugin

--- src/builtin_plugins/amulet_team_3d_viewer/_plugin.py
+++ src/builtin_plugins/amulet_team_3d_viewer/_plugin.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import os
 from typing import Optional
+from contextlib import suppress
 
 from PySide6.QtCore import QLocale, QCoreApplication
 
@@ -9,8 +10,7 @@ from amulet_editor.models.localisation import ATranslator
 from amulet_editor.models.plugin import PluginV1
 
 import amulet_team_locale
-import amulet_team_main_window
-import tablericons
+from amulet_team_main_window import register_widget, unregister_widget
 
 import amulet_team_3d_viewer
 from ._view_3d import View3D
@@ -18,29 +18,20 @@ from ._view_3d import View3D
 
 # Qt only weekly references this. We must hold a strong reference to stop it getting garbage collected
 _translator: Optional[ATranslator] = None
-view_3d_button: Optional[amulet_team_main_window.ButtonProxy] = None
 
 
-def _set_view_3d_layout():
-    amulet_team_main_window.get_main_window().set_layout(View3D)
-
-
-def load_plugin():
-    global _translator, view_3d_button
+def load_plugin() -> None:
+    global _translator
     if get_level() is not None:
         _translator = ATranslator()
         _locale_changed()
         QCoreApplication.installTranslator(_translator)
         amulet_team_locale.locale_changed.connect(_locale_changed)
-        amulet_team_main_window.register_widget(View3D)
 
-        view_3d_button = amulet_team_main_window.add_toolbar_button(sticky=True)
-        view_3d_button.set_icon(tablericons.three_d_cube_sphere)
-        view_3d_button.set_name("3D Editor")
-        view_3d_button.set_callback(_set_view_3d_layout)
+        register_widget(View3D)
 
 
-def _locale_changed():
+def _locale_changed() -> None:
     assert _translator is not None
     _translator.load_lang(
         QLocale(),
@@ -49,8 +40,11 @@ def _locale_changed():
     )
 
 
-def unload_plugin():
-    QCoreApplication.removeTranslator(_translator)
+def unload_plugin() -> None:
+    with suppress(ValueError):
+        unregister_widget(View3D)
+    if _translator is not None:
+        QCoreApplication.removeTranslator(_translator)
 
 
 plugin = PluginV1(load=load_plugin, unload=unload_plugin)
