f854010b3face1e24bb7afe87b4b3fbbdb48bae9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-25 16:57:16 +0100

    Added chunk builder stub file

---
+++ src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_builder.pyi
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import amulet.chunk_components
+import amulet.palette.block_palette
+import amulet_team_3d_viewer._view_3d
+
+__all__ = ["create_lod0_chunk"]
+
+def create_lod0_chunk(
+    arg0: amulet_team_3d_viewer._view_3d._resource_pack,
+    arg1: amulet.chunk_components.IndexArray3D,
+    arg2: amulet.palette.block_palette.BlockPalette,
+) -> None: ...
