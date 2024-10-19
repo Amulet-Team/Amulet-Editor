a0df25d58907e2bf154c193e515b6d1ffec0835d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-10-19 14:23:30 +0100

    Modified python function signature

--- src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.py.cpp
+++ src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.py.cpp
@@ -9,6 +9,8 @@
 
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/typing.h>
 
 #include <amulet/block.hpp>
 #include <amulet/chunk_components/section_array_map.hpp>
@@ -21,36 +23,56 @@
 namespace py = pybind11;
 
 
-static void _create_lod0_chunk(
-    Amulet::AbstractOpenGLResourcePack& resource_pack,
-    const std::int64_t cx,
-    const std::int64_t cz,
-    const Amulet::ChunkData& all_chunk_data,
-    std::string& opaque_buffer,
-    std::string& translucent_buffer)
-{
-    py::gil_scoped_release gil;
-    Amulet::create_lod0_chunk(resource_pack, cx, cz, all_chunk_data, opaque_buffer, translucent_buffer);
-} 
-
-
 void init_chunk_mesher(py::module m_parent)
 {
-    auto m = m_parent.def_submodule("_chunk_mesher_lod0");
-    py::module::import("amulet.palette.block_palette");
-    m.def(
-        "create_lod0_chunk",
-        [](
-            Amulet::AbstractOpenGLResourcePack& resource_pack,
-            const std::int64_t cx,
-            const std::int64_t cz,
-            const Amulet::ChunkData& all_chunk_data) -> std::pair<py::bytes, py::bytes> {
-            std::string opaque_buffer;
-            std::string translucent_buffer;
+	auto m = m_parent.def_submodule("_chunk_mesher_lod0");
+	py::module::import("amulet.palette.block_palette");
+	m.def(
+		"create_lod0_chunk",
+		[](
+			Amulet::AbstractOpenGLResourcePack& resource_pack,
+			const std::int64_t cx,
+			const std::int64_t cz,
+			const Amulet::BlockComponentData& py_chunk_component,
+			Amulet::pybind11::type_hints::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_north_chunk_component,
+			Amulet::pybind11::type_hints::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_east_chunk_component,
+			Amulet::pybind11::type_hints::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_south_chunk_component,
+			Amulet::pybind11::type_hints::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_west_chunk_component
+			) -> std::pair<py::bytes, py::bytes> {
+				std::string opaque_buffer;
+				std::string translucent_buffer;
 
-            _create_lod0_chunk(resource_pack, cx, cz, all_chunk_data, opaque_buffer, translucent_buffer);
+				auto get_chunk_data = [&](py::object py_obj) -> const Amulet::BlockComponentData* const {
+					if (py_obj.is_none()) {
+						return nullptr;
+					}
+					else {
+						return &py_obj.cast<const Amulet::BlockComponentData&>();
+					}
+					};
 
-            return std::make_pair(py::bytes(opaque_buffer), py::bytes(translucent_buffer));
-        }
-    );
+				Amulet::ChunkData all_chunk_data({
+					get_chunk_data(py_north_chunk_component),
+					get_chunk_data(py_west_chunk_component),
+					&py_chunk_component,
+					get_chunk_data(py_east_chunk_component),
+					get_chunk_data(py_south_chunk_component),
+					});
+
+				{
+					py::gil_scoped_release gil;
+					Amulet::create_lod0_chunk(resource_pack, cx, cz, all_chunk_data, opaque_buffer, translucent_buffer);
+				}
+
+				return std::make_pair(py::bytes(opaque_buffer), py::bytes(translucent_buffer));
+		},
+		py::arg("resource_pack"),
+		py::arg("cx"),
+		py::arg("cz"),
+		py::arg("block_component"),
+		py::arg("north_block_component"),
+		py::arg("east_block_component"),
+		py::arg("south_block_component"),
+		py::arg("west_block_component")
+	);
 }
--- src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.pyi
+++ src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.pyi
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
 import amulet.chunk_components
-import amulet.palette.block_palette
 import amulet_team_3d_viewer._view_3d._resource_pack_base
 
 __all__ = ["create_lod0_chunk"]
 
 def create_lod0_chunk(
-    arg0: amulet_team_3d_viewer._view_3d._resource_pack_base.AbstractOpenGLResourcePack,
-    arg1: int,
-    arg2: int,
-    arg3: amulet.chunk_components.SectionArrayMap,
-    arg4: amulet.palette.block_palette.BlockPalette,
+    resource_pack: amulet_team_3d_viewer._view_3d._resource_pack_base.AbstractOpenGLResourcePack,
+    cx: int,
+    cz: int,
+    block_component: amulet.chunk_components.BlockComponentData,
+    north_block_component: amulet.chunk_components.BlockComponentData | None,
+    east_block_component: amulet.chunk_components.BlockComponentData | None,
+    south_block_component: amulet.chunk_components.BlockComponentData | None,
+    west_block_component: amulet.chunk_components.BlockComponentData | None,
 ) -> tuple[bytes, bytes]: ...
