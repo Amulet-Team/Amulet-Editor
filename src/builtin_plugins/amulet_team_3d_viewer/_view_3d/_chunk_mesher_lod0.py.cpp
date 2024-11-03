d405a506c789b485eea162ca8fbba35d07017dac
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-03 16:10:27 +0000

    Removed builtins namespace

--- src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.py.cpp
+++ src/builtin_plugins/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.py.cpp
@@ -34,10 +34,10 @@ void init_chunk_mesher(py::module m_parent)
 			const std::int64_t cx,
 			const std::int64_t cz,
 			const Amulet::BlockComponentData& py_chunk_component,
-			pybind11_extensions::builtins::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_north_chunk_component,
-            pybind11_extensions::builtins::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_east_chunk_component,
-            pybind11_extensions::builtins::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_south_chunk_component,
-            pybind11_extensions::builtins::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_west_chunk_component
+			pybind11_extensions::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_north_chunk_component,
+            pybind11_extensions::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_east_chunk_component,
+            pybind11_extensions::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_south_chunk_component,
+            pybind11_extensions::PyObjectCpp<std::optional<Amulet::BlockComponentData>> py_west_chunk_component
 			) -> std::pair<py::bytes, py::bytes> {
 				std::string opaque_buffer;
 				std::string translucent_buffer;
