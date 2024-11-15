1c5a971a3fcedea0d6f7e4e32eaa61a45d505c02
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-15 11:47:02 +0000

    Reformatted

--- tools/generate_pybind_stubs.py
+++ tools/generate_pybind_stubs.py
@@ -3,6 +3,7 @@ import importlib.util
 
 from shared.generate_pybind_stubs import run
 
+
 def get_module_path(name: str) -> str:
     spec = importlib.util.find_spec(name)
     assert spec is not None
@@ -10,13 +11,16 @@ def get_module_path(name: str) -> str:
     assert module_path is not None
     return module_path
 
+
 def get_package_dir(name: str) -> str:
     return os.path.dirname(get_module_path(name))
+
 
 def main():
     path = get_package_dir("amulet_team_3d_viewer")
     src_path = os.path.dirname(path)
     run(src_path, "amulet_team_3d_viewer._view_3d")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
