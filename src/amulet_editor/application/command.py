15df2764592e5ae215a74387ac22bd3d4695f921
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-01 19:55:07 +0100

    Expanded the command module

--- src/amulet_editor/application/command.py
+++ src/amulet_editor/application/command.py
@@ -3,37 +3,66 @@ Launch commands that can be executed through the CLI.
 Plugins can register their own launch commands.
 """
 
-import argparse
+from argparse import ArgumentParser, Namespace
 from typing import Any
-from weakref import WeakSet
-from collections.abc import Callable
+from weakref import WeakSet, WeakValueDictionary
+from collections.abc import Callable, Mapping, Sequence
 from dataclasses import dataclass
 from threading import Lock
+from types import MappingProxyType
 
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, kw_only=True)
 class Command:
     name: str
-    main_func: Callable[[Any], None]
-    init_argparse: Callable[[argparse.ArgumentParser], None] | None = None
+    main_func: Callable[[Namespace], None]
+    init_argparse: Callable[[ArgumentParser], None] | None = None
+    add_parser_args: Sequence[Any] = ()
+    add_parser_kwargs: Mapping[str, Any] = MappingProxyType({})
+
+    def __hash__(self) -> int:
+        return id(self)
 
 
 _lock = Lock()
-_commands = WeakSet[Command]()
+_commands_set = WeakSet[Command]()
+_commands_map = WeakValueDictionary[str, Command]()
 
 
 def register_command(command: Command) -> None:
+    """
+    Register a command. The name must be unique.
+    The Command object must be kept alive by the caller.
+    """
     if not isinstance(command, Command):
         raise TypeError(f"Command {command} is not a Command")
     with _lock:
-        if any(c.name == command.name for c in _commands):
+        if command.name in _commands_map:
             raise RuntimeError(f"Command {command.name} has already been registered.")
-        _commands.add(command)
+        _commands_set.add(command)
 
 
 def unregister_command(command: Command) -> None:
+    """
+    Unregister a command.
+    If the Command object was not previously registered, this will do nothing.
+    """
     if not isinstance(command, Command):
         raise TypeError(f"Command {command} is not a Command")
     with _lock:
-        if command in _commands:
-            _commands.remove(command)
+        if command in _commands_set:
+            _commands_set.remove(command)
+            _commands_map.pop(command.name)
+
+
+def _get_commands() -> list[Command]:
+    with _lock:
+        return list(_commands_set)
+
+
+def _run_command(name: str, args: Namespace) -> None:
+    with _lock:
+        command = _commands_map.get(name)
+    if command is None:
+        raise RuntimeError(f'Could not find the "{name}" command.')
+    command.main_func(args)
