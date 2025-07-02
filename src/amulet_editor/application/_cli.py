85cb1b26a6ff4f24d05f5125b2ffcfc175523366
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-02 09:55:14 +0100

    Refactor the argument parser

--- src/amulet_editor/application/_cli.py
+++ src/amulet_editor/application/_cli.py
@@ -1,8 +1,8 @@
-import argparse
+from argparse import ArgumentParser, Namespace
 import logging
 import subprocess
 import sys
-from typing import Protocol, Optional
+from collections.abc import Sequence
 from amulet_editor.data.paths._application import (
     DefaultDataDir,
     DefaultConfigDir,
@@ -10,107 +10,123 @@ from amulet_editor.data.paths._application import (
     DefaultLogDir,
 )
 
+from . import command as command_mod
+
 
 BROKER = "BROKER"
 
 
-class Args(Protocol):
-    level_path: Optional[str]
-    data_dir: Optional[str]
-    config_dir: Optional[str]
-    cache_dir: Optional[str]
-    log_dir: Optional[str]
+class GlobalArgs(Namespace):
+    data_dir: str | None
+    config_dir: str | None
+    cache_dir: str | None
+    log_dir: str | None
     logging_level: int
     logging_format: str
     trace: bool
 
 
-_args: Optional[Args] = None
+class FullArgs(GlobalArgs):
+    command: str | None
+    # level_path: str | None
 
 
-def parse_args() -> Args:
-    global _args
+def get_parser(full: bool) -> ArgumentParser:
+    parser = ArgumentParser(
+        prog="amulet_editor",
+        add_help=full,
+        description="Amulet is a Minecraft world editing application.",
+    )
 
-    if _args is None:
-        parser = argparse.ArgumentParser(
-            prog="Amulet Editor",
-            description="Amulet is a Minecraft world editing application.",
-            epilog="The following arguments are a subset of the full arguments.",
+    parser.add_argument(
+        "--data_dir",
+        type=str,
+        help=f"The directory to store application data in. Default is {DefaultDataDir}",
+        action="store",
+        dest="data_dir",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--config_dir",
+        type=str,
+        help=f"The directory to store application config files in. Default is {DefaultConfigDir}",
+        action="store",
+        dest="config_dir",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--cache_dir",
+        type=str,
+        help=f"The directory to store cache files in. Default is {DefaultCacheDir}",
+        action="store",
+        dest="cache_dir",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--log_dir",
+        type=str,
+        help=f"The directory to store log files in. Default is {DefaultLogDir}",
+        action="store",
+        dest="log_dir",
+        default=None,
+    )
+
+    parser.add_argument(
+        "--logging_level",
+        type=int,
+        help="The logging level to set. CRITICAL=50, ERROR=40, WARNING=30, INFO=20, DEBUG=10. Default is WARNING",
+        action="store",
+        dest="logging_level",
+        default=logging.WARNING,
+    )
+
+    parser.add_argument(
+        "--logging_format",
+        type=str,
+        help='The logging format to use. Default is "%(levelname)s - %(message)s"',
+        action="store",
+        dest="logging_format",
+        default="%(levelname)s - %(message)s",
+    )
+
+    parser.add_argument(
+        "--trace",
+        help="If defined, print the qualified name of each function as it is called. Useful if the program crashes due to an access violation and you don't know where it came from.",
+        action="store_true",
+        dest="trace",
+    )
+
+    if full:
+        entry_subcommand = parser.add_subparsers(
+            dest="command", help="The main command to run"
         )
 
-        parser.add_argument(
-            "--level_path",
-            type=str,
-            help="The Minecraft world or structure to open. Default opens no level",
-            action="store",
-            dest="level_path",
-            default=None,
-        )
+        for command in command_mod._get_commands():
+            command_parser = entry_subcommand.add_parser(
+                command.name,
+                *command.add_parser_args,
+                **command.add_parser_kwargs,
+            )
+            init_argparse = command.init_argparse
+            if init_argparse is not None:
+                init_argparse(command_parser)
 
-        parser.add_argument(
-            "--data_dir",
-            type=str,
-            help=f"The directory to store application data in. Default is {DefaultDataDir}",
-            action="store",
-            dest="data_dir",
-            default=None,
-        )
+    return parser
 
-        parser.add_argument(
-            "--config_dir",
-            type=str,
-            help=f"The directory to store application config files in. Default is {DefaultConfigDir}",
-            action="store",
-            dest="config_dir",
-            default=None,
-        )
 
-        parser.add_argument(
-            "--cache_dir",
-            type=str,
-            help=f"The directory to store cache files in. Default is {DefaultCacheDir}",
-            action="store",
-            dest="cache_dir",
-            default=None,
-        )
+def parse_global_args(argv: Sequence[str] | None = None) -> GlobalArgs:
+    parser = get_parser(False)
+    args, _ = parser.parse_known_args(argv)
+    return args  # noqa
 
-        parser.add_argument(
-            "--log_dir",
-            type=str,
-            help=f"The directory to store log files in. Default is {DefaultLogDir}",
-            action="store",
-            dest="log_dir",
-            default=None,
-        )
 
-        parser.add_argument(
-            "--logging_level",
-            type=int,
-            help="The logging level to set. CRITICAL=50, ERROR=40, WARNING=30, INFO=20, DEBUG=10. Default is WARNING",
-            action="store",
-            dest="logging_level",
-            default=logging.WARNING,
-        )
-
-        parser.add_argument(
-            "--logging_format",
-            type=str,
-            help='The logging format to use. Default is "%(levelname)s - %(message)s"',
-            action="store",
-            dest="logging_format",
-            default="%(levelname)s - %(message)s",
-        )
-
-        parser.add_argument(
-            "--trace",
-            help="If defined, print the qualified name of each function as it is called. Useful if the program crashes due to an access violation and you don't know where it came from.",
-            action="store_true",
-            dest="trace",
-        )
-
-        _args, _ = parser.parse_known_args()
-
-    return _args  # noqa
+def parse_args(argv: Sequence[str] | None = None) -> FullArgs:
+    parser = get_parser(True)
+    args, _ = parser.parse_known_args(argv)
+    return args  # noqa
 
 
 # TODO: move this somewhere more sensible
@@ -118,8 +134,8 @@ def spawn_process(path: str | None = None) -> None:
     """Spawn the broker process passing over the input CLI values."""
     this_args = parse_args()
     new_args = [sys.executable, sys.argv[0]]
-    if path is not None:
-        new_args += ["--level_path", path]
+    # if path is not None:
+    #     new_args += ["--level_path", path]
     new_args += [
         "--logging_level",
         str(this_args.logging_level),
