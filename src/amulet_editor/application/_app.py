3b472c8e9dcaab1229f21f02d00f3cc78d1b847a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-02 09:54:47 +0100

    Remove AmuletApp

--- src/amulet_editor/application/_app.py
+++ src/amulet_editor/application/_app.py
@@ -16,44 +16,43 @@ import amulet_editor.data.plugin._manager as plugin_manager
 
 from . import appearance
 
-
-class AmuletApp(QApplication):
-    def __init__(self) -> None:
-        super().__init__()
-        self.setApplicationName("Amulet Editor")
-        self.setApplicationVersion(__version__)
-        self.setWindowIcon(QIcon(get_resource("icons/amulet/Icon.ico")))
-
-        self._translator = ATranslator()
-        self._locale_changed()
-        QCoreApplication.installTranslator(self._translator)
-        locale_changed.connect(self._locale_changed)
-
-        appearance.theme().apply(self)
-
-        self.lastWindowClosed.connect(self._last_window_closed)
-        QTimer.singleShot(0, plugin_manager.load)
-
-    @staticmethod
-    def instance() -> Optional[AmuletApp]:
-        instance = QApplication.instance()
-        assert isinstance(instance, AmuletApp)
-        return instance
-
-    @Slot()
-    def _last_window_closed(self) -> None:
-        # The unload method opens a window and then closes it.
-        # We must unbind this signal so that it does not end in a loop.
-        self.lastWindowClosed.disconnect(self._last_window_closed)
-        # unload all the plugins
-        plugin_manager.unload()
-        # Forcefully quit the application just in case a plugin opened a window during unload.
-        self.quit()
-
-    @Slot()
-    def _locale_changed(self) -> None:
-        self._translator.load_lang(
-            QLocale(),
-            "",
-            directory=os.path.join(*amulet_editor.__path__, "resources", "lang"),
-        )
+# class AmuletApp(QApplication):
+#     def __init__(self) -> None:
+#         super().__init__()
+#         self.setApplicationName("Amulet Editor")
+#         self.setApplicationVersion(__version__)
+#         self.setWindowIcon(QIcon(get_resource("icons/amulet/Icon.ico")))
+#
+#         self._translator = ATranslator()
+#         self._locale_changed()
+#         QCoreApplication.installTranslator(self._translator)
+#         locale_changed.connect(self._locale_changed)
+#
+#         appearance.theme().apply(self)
+#
+#         self.lastWindowClosed.connect(self._last_window_closed)
+#         QTimer.singleShot(0, plugin_manager.load)
+#
+#     @staticmethod
+#     def instance() -> Optional[AmuletApp]:
+#         instance = QApplication.instance()
+#         assert isinstance(instance, AmuletApp)
+#         return instance
+#
+#     @Slot()
+#     def _last_window_closed(self) -> None:
+#         # The unload method opens a window and then closes it.
+#         # We must unbind this signal so that it does not end in a loop.
+#         self.lastWindowClosed.disconnect(self._last_window_closed)
+#         # unload all the plugins
+#         plugin_manager.unload()
+#         # Forcefully quit the application just in case a plugin opened a window during unload.
+#         self.quit()
+#
+#     @Slot()
+#     def _locale_changed(self) -> None:
+#         self._translator.load_lang(
+#             QLocale(),
+#             "",
+#             directory=os.path.join(*amulet_editor.__path__, "resources", "lang"),
+#         )
