f200c21013a5c23675335eeb0d093f14370d6ec4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-03 14:10:43 +0100

    Rename amulet_editor to amulet.app
    The amulet_editor module is no longer the actual editor so this name is confusing.
    The editor is implemented through plugins.

--- src/amulet/app/__pyinstaller/hook-amulet.app.py
+++ src/amulet/app/__pyinstaller/hook-amulet.app.py
@@ -5,17 +5,17 @@ from PyInstaller.utils.hooks import (
 )
 
 datas = [
-    *collect_data_files("amulet_editor", excludes=["**/*.ui", "**/*.cpp", "**/*.pyc"]),
+    *collect_data_files("amulet.app", excludes=["**/*.ui", "**/*.cpp", "**/*.pyc"]),
     *collect_data_files(
         "builtin_plugins",
         include_py_files=True,
         excludes=["**/*.ui", "**/*.cpp", "**/*.pyc"],
     ),
-    *copy_metadata("amulet_editor", recursive=True),
+    *copy_metadata("amulet.app", recursive=True),
 ]
 
 hiddenimports = [
-    *collect_submodules("amulet_editor"),
+    *collect_submodules("amulet.app"),
     "PySide6",
     "OpenGL",
 ]
