f32efad4911e80210589e42d31efa2fd3dd12bcd
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-07-09 08:12:48 +0100

    Reformat

--- src/builtin_plugins/plugin/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.pyi
+++ src/builtin_plugins/plugin/amulet_team_3d_viewer/_view_3d/_chunk_mesher_lod0.pyi
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 import amulet.core.chunk.component
-from plugin.amulet_team_3d_viewer._view_3d._resource_pack_base import AbstractOpenGLResourcePack
+from plugin.amulet_team_3d_viewer._view_3d._resource_pack_base import (
+    AbstractOpenGLResourcePack,
+)
 
 __all__ = ["create_lod0_chunk"]
 
