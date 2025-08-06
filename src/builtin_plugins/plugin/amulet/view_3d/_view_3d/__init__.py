06ccd5c4716db160143e0f2660de1670fd263cd8
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-08-06 11:00:54 +0100

    Add imports

--- src/builtin_plugins/plugin/amulet/view_3d/_view_3d/__init__.py
+++ src/builtin_plugins/plugin/amulet/view_3d/_view_3d/__init__.py
@@ -2,6 +2,11 @@ def _init() -> None:
     import sys
 
     import PySide6.QtOpenGL
+    import amulet.utils
+    import amulet.core
+    import amulet.game
+    import amulet.level
+    import amulet.resource_pack
 
     from ._view_3d import init
 
