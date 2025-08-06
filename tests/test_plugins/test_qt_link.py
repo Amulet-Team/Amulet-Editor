583fbc570ceac62d287165f16040d69e87d607d6
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-08-06 11:02:30 +0100

    Reformat

--- tests/test_plugins/test_qt_link.py
+++ tests/test_plugins/test_qt_link.py
@@ -15,6 +15,7 @@ class TestQtLink(unittest.TestCase):
     def test_qt_link(self) -> None:
         from PySide6 import __version__ as qt_version
         from plugin.amulet.view_3d._view_3d import _get_qt_version
+
         self.assertEqual(qt_version, _get_qt_version())
 
 
