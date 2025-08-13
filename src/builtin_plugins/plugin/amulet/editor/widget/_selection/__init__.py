ac72301e9d0f8bb48889dbc5aeea48264ba0013a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-08-13 10:42:12 +0100

    Add dummy selection widget

---
+++ src/builtin_plugins/plugin/amulet/editor/widget/_selection/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import Optional
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QWidget, QVBoxLayout
+
+from plugin.amulet.editor.window import TabWidget
+
+
+class SelectionWidget(TabWidget):
+    name = "Selection"
+
+    def __init__(
+        self, parent: Optional[QWidget] = None, f: Qt.WindowType = Qt.WindowType.Widget
+    ):
+        super().__init__(parent, f)
+        self._layout = QVBoxLayout(self)
+        self._layout.setContentsMargins(0, 0, 0, 0)
