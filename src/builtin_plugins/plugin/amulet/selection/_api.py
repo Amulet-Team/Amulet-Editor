58822dc5bc2c3d00ee4b29f8398e4f788287922d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-01 15:25:13 +0100

    Redesign selection plugin
    The selection can now be a box group or shape group

--- src/builtin_plugins/plugin/amulet/selection/_api.py
+++ src/builtin_plugins/plugin/amulet/selection/_api.py
@@ -1,17 +1,22 @@
-from amulet.core.selection import SelectionGroup
+from threading import Lock
+
+from amulet.core.selection import SelectionBoxGroup, SelectionShapeGroup
 from amulet.app._signal import create_signal
 
+_lock = Lock()
 
-_selection: SelectionGroup = SelectionGroup()
+_selection: SelectionBoxGroup | SelectionShapeGroup = SelectionBoxGroup()
 
-_selection_changed_obj, selection_changed = create_signal(SelectionGroup)
-
-
-def get_selection() -> SelectionGroup:
-    return _selection
+_selection_changed_obj, selection_changed = create_signal(SelectionBoxGroup | SelectionShapeGroup)
 
 
-def set_selection(selection: SelectionGroup) -> None:
+def get_selection() -> SelectionBoxGroup | SelectionShapeGroup:
+    with _lock:
+        return _selection
+
+
+def set_selection(selection: SelectionBoxGroup | SelectionShapeGroup) -> None:
     global _selection
-    _selection = selection
+    with _lock:
+        _selection = selection
     selection_changed.emit(selection)
