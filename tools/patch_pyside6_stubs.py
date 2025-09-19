effa09258d04818823ccd310ef1d1dae120c3c53
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-09-19 11:11:35 +0100

    Fix the pyside6 stub patcher for new versions

--- tools/patch_pyside6_stubs.py
+++ tools/patch_pyside6_stubs.py
@@ -6,32 +6,35 @@ PySide6Path = PySide6.__path__[0]
 Patches: dict[str, list[tuple[str, str]]] = {
     os.path.join(PySide6Path, "QtCore.pyi"): [
         (
-            "def translate(self, context: Union[bytes, bytearray, memoryview], sourceText: Union[bytes, bytearray, memoryview], disambiguation: Union[bytes, bytearray, memoryview, NoneType] = ..., n: int = ...) -> str: ...",
-            "def translate(self, context: str, sourceText: str, disambiguation: Union[str, NoneType] = ..., n: int = ...) -> Union[str, None]: ...",
-        ),
-        (
-            "def data(self) -> Union[bytes, bytearray, memoryview]: ...",
-            "def data(self) -> Union[bytes]: ...",
+            # QByteArray
+            "def data(self, /) -> bytes | bytearray | memoryview: ...",
+            "def data(self) -> bytes: ...",
         ),
     ],
     os.path.join(PySide6Path, "QtGui.pyi"): [
         (
-            "def setParent(self, parent: PySide6.QtGui.QWindow",
-            "def setParent(self, parent: Optional[PySide6.QtGui.QWindow]",
+            # QWindow
+            "def setParent(self, parent: PySide6.QtGui.QWindow, /) -> None: ...",
+            "def setParent(self, parent: PySide6.QtGui.QWindow | None, /) -> None: ...",
         ),
         (
-            "def glVertexAttribPointer(self, indx: int, size: int, type: int, normalized: int, stride: int, ptr: int) -> None: ...",
-            "def glVertexAttribPointer(self, indx: int, size: int, type: int, normalized: int, stride: int, ptr: Shiboken.VoidPtr) -> None: ...",
+            # QOpenGLFunctions
+            "def glVertexAttribPointer(self, indx: int, size: int, type: int, normalized: int, stride: int, ptr: int, /) -> None: ...",
+            "def glVertexAttribPointer(self, indx: int, size: int, type: int, normalized: int, stride: int, ptr: Shiboken.VoidPtr, /) -> None: ...",
         ),
     ],
     os.path.join(PySide6Path, "QtWidgets.pyi"): [
         (
-            "def spacerItem(self) -> PySide6.QtWidgets.QSpacerItem: ...\n    def widget(self) -> PySide6.QtWidgets.QWidget: ...",
-            "def spacerItem(self) -> Optional[PySide6.QtWidgets.QSpacerItem]: ...\n    def widget(self) -> Optional[PySide6.QtWidgets.QWidget]: ...",
+            # QLayoutItem
+            "    def spacerItem(self, /) -> PySide6.QtWidgets.QSpacerItem: ...\n"
+            "    def widget(self, /) -> PySide6.QtWidgets.QWidget: ...",
+            "    def spacerItem(self, /) -> PySide6.QtWidgets.QSpacerItem | None: ...\n"
+            "    def widget(self, /) -> PySide6.QtWidgets.QWidget | None: ...",
         ),
         (
-            "def itemAt(self, arg__1: int) -> PySide6.QtWidgets.QLayoutItem: ...",
-            "def itemAt(self, arg__1: int) -> Optional[PySide6.QtWidgets.QLayoutItem]: ...",
+            # QStackedLayout
+            "def itemAt(self, arg__1: int, /) -> PySide6.QtWidgets.QLayoutItem: ...",
+            "def itemAt(self, index: int, /) -> PySide6.QtWidgets.QLayoutItem | None: ...",
         ),
     ],
 }
