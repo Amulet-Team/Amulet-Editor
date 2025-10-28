955b78003dafaf1720992ffa39db386cd1f8343b
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-10-28 16:44:15 +0000

    Add arrow buttons and fix highlighting
    Add arrow buttons when the widget gets too small for those that do not have mouse wheels.
    Fix button highlighting when scrolling. When scrolling the buttons do not get the mouse leave event so do not get unhighlighted.

--- src/builtin_plugins/plugin/amulet/editor/window/_toolbar.py
+++ src/builtin_plugins/plugin/amulet/editor/window/_toolbar.py
@@ -2,19 +2,31 @@ from typing import Callable
 import traceback
 from weakref import finalize, WeakMethod
 
-from PySide6.QtCore import QSize, Qt, QObject, QEvent, QRect, QPoint
-from PySide6.QtGui import QMouseEvent
+from PySide6.QtCore import QSize, Qt, QObject, QEvent, QPoint
+from PySide6.QtGui import (
+    QMouseEvent,
+    QResizeEvent,
+    QWheelEvent,
+    QCursor,
+    QHoverEvent,
+    QEnterEvent,
+)
 from PySide6.QtWidgets import (
     QFrame,
     QWidget,
+    QBoxLayout,
     QVBoxLayout,
     QHBoxLayout,
     QButtonGroup,
     QScrollArea,
+    QPushButton,
+    QApplication,
 )
 
-from plugin.amulet.editor._toolbar_button import ToolbarButton
+from amulet.app.qt.signal import Signal
 from amulet.app.exception import display_exception
+
+from plugin.amulet.editor._toolbar_button import ToolbarButton
 
 
 class ButtonProxy:
@@ -93,6 +105,7 @@ class DynamicButtonWidget(QScrollArea):
     """A rearrangeable list of buttons."""
 
     # orderChanged = Signal(list)
+    resized = Signal[()]()
 
     def __init__(
         self,
@@ -107,14 +120,13 @@ class DynamicButtonWidget(QScrollArea):
         else:
             self._size_hint = QSize(ButtonSize * 2, ButtonSize)
 
-        # self.setMouseTracking(True)
-
         self._widget = QWidget()
 
         layout_cls = LayoutCls[orientation]
 
         self._container_layout = layout_cls(self._widget)
         self._container_layout.setContentsMargins(0, 0, 0, 0)
+        self._container_layout.setSpacing(2)
 
         self._layout = layout_cls()
         self._layout.setContentsMargins(0, 0, 0, 0)
@@ -131,6 +143,25 @@ class DynamicButtonWidget(QScrollArea):
         self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.setWidgetResizable(True)
+
+    def child_widget(self) -> QWidget:
+        return self._widget
+
+    def _find_widget_at(self, point: QPoint) -> tuple[int, QWidget] | None:
+        """
+        Find the child widget of _widget that intersects with the given point.
+        point is relative to _widget.
+        """
+        for i in range(self._layout.count()):
+            item = self._layout.itemAt(i)
+            if item is None:
+                continue
+            child = item.widget()
+            if child is None:
+                continue
+            if child.geometry().contains(point):
+                return i, child
+        return None
 
     def eventFilter(self, obj: QObject, event: QEvent) -> bool:
         if isinstance(event, QMouseEvent) and isinstance(obj, QWidget):
@@ -149,23 +180,17 @@ class DynamicButtonWidget(QScrollArea):
                     if dragged:
                         return True
                 elif event.type() == QEvent.Type.MouseMove:
-                    point = event.globalPos()
-                    for i in range(self._layout.count()):
-                        item = self._layout.itemAt(i)
-                        if item is None:
-                            continue
-                        child = item.widget()
-                        if child is None:
-                            continue
-                        if QRect(child.mapToGlobal(QPoint()), child.size()).contains(
-                            point
-                        ):
-                            if self._dragged_widget is child:
-                                break
+                    found = self._find_widget_at(
+                        self._widget.mapFromGlobal(event.globalPos())
+                    )
+                    if found is not None:
+                        i, child = found
+                        if self._dragged_widget is not child:
                             self._layout.removeWidget(self._dragged_widget)
                             self._layout.insertWidget(i, self._dragged_widget)
                             self._dragged = True
-                            break
+        elif obj is self._widget and isinstance(event, QResizeEvent):
+            self.resized.emit()
         return super().eventFilter(obj, event)
 
     def add_item(self, item: QWidget) -> None:
@@ -178,6 +203,71 @@ class DynamicButtonWidget(QScrollArea):
     def minimumSizeHint(self) -> QSize:
         return self._size_hint
 
+    def resizeEvent(self, event: QResizeEvent) -> None:
+        super().resizeEvent(event)
+        self.resized.emit()
+
+    def wheelEvent(self, event: QWheelEvent) -> None:
+        # Enter and leave events are not generated automatically when scrolling.
+        # This manually generates them.
+
+        # Store which widget the mouse is over
+        global_pos = QCursor.pos()
+        old_pos = self._widget.mapFromGlobal(global_pos)
+        old_state = self._find_widget_at(old_pos)
+        old_widget = None if old_state is None else old_state[1]
+
+        bar = (
+            self.horizontalScrollBar()
+            if self._orientation == Qt.Orientation.Horizontal
+            else self.verticalScrollBar()
+        )
+        bar.setValue(bar.value() - event.angleDelta().y() // 5)
+
+        # Get the new widget the mouse is over
+        new_pos = self._widget.mapFromGlobal(global_pos)
+        new_state = self._find_widget_at(new_pos)
+        new_widget = None if new_state is None else new_state[1]
+        # If the mouse is over a different widget manually generate the leave and enter events.
+        # Qt does not generate them automatically for some reason.
+        if old_widget != new_widget:
+            if old_widget is not None:
+                QApplication.sendEvent(old_widget, QEvent(QEvent.Type.Leave))
+                QApplication.sendEvent(
+                    old_widget,
+                    QHoverEvent(
+                        QEvent.Type.HoverLeave,
+                        new_pos,
+                        global_pos,
+                        old_pos,
+                        Qt.KeyboardModifier.NoModifier,
+                    ),
+                )
+            if new_widget is not None:
+                QApplication.sendEvent(
+                    new_widget,
+                    QEnterEvent(
+                        new_pos,
+                        new_pos,
+                        global_pos,
+                    ),
+                )
+                QApplication.sendEvent(
+                    new_widget,
+                    QHoverEvent(QEvent.Type.HoverEnter, new_pos, global_pos, old_pos),
+                )
+
+    def scroll_up(self) -> None:
+        if self._orientation == Qt.Orientation.Vertical:
+            self.verticalScrollBar().setValue(self.verticalScrollBar().value() - 60)
+        else:
+            self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() - 60)
+
+    def scroll_down(self) -> None:
+        if self._orientation == Qt.Orientation.Vertical:
+            self.verticalScrollBar().setValue(self.verticalScrollBar().value() + 60)
+        else:
+            self.horizontalScrollBar().setValue(self.horizontalScrollBar().value() + 60)
 
 
 class ToolBar(QWidget):
@@ -191,20 +281,66 @@ class ToolBar(QWidget):
         orientation: Qt.Orientation = Qt.Orientation.Vertical,
     ) -> None:
         super().__init__()
-
+        self._orientation = orientation
         layout_cls = LayoutCls[orientation]
 
         self._main_layout = layout_cls(self)
-        self._main_layout.setSpacing(5)
+        self._main_layout.setSpacing(2)
         self._main_layout.setContentsMargins(0, 0, 0, 0)
 
         self._dynamic_button_widget = DynamicButtonWidget(self, orientation)
         self._main_layout.addWidget(self._dynamic_button_widget, 1)
 
+        self._arrow_widget = QWidget()
+        self._main_layout.addWidget(self._arrow_widget)
+
+        if orientation == Qt.Orientation.Vertical:
+            direction = QBoxLayout.Direction.LeftToRight
+            up_label = "˄"
+            down_label = "˅"
+        else:
+            direction = QBoxLayout.Direction.TopToBottom
+            up_label = "˂"
+            down_label = "˃"
+
+        self._arrow_layout = QBoxLayout(direction, self._arrow_widget)
+        self._arrow_layout.setContentsMargins(0, 0, 0, 0)
+        self._arrow_layout.setSpacing(0)
+
+        self._up_arrow = QPushButton(up_label)
+        self._up_arrow.setFixedSize(ButtonSize // 2, ButtonSize // 2)
+        self._up_arrow.clicked.connect(self._dynamic_button_widget.scroll_up)
+        self._arrow_layout.addWidget(self._up_arrow)
+
+        self._down_arrow = QPushButton(down_label)
+        self._down_arrow.setFixedSize(ButtonSize // 2, ButtonSize // 2)
+        self._down_arrow.clicked.connect(self._dynamic_button_widget.scroll_down)
+        self._arrow_layout.addWidget(self._down_arrow)
+
+        size_policy = self._arrow_widget.sizePolicy()
+        size_policy.setRetainSizeWhenHidden(True)
+        self._arrow_widget.setSizePolicy(size_policy)
+        self._dynamic_button_widget.resized.connect(self._on_dynamic_resize)
+
         self._static_button_layout = layout_cls()
         self._main_layout.addLayout(self._static_button_layout)
 
-        self._layout_button_group = QButtonGroup()
+        self._button_group = QButtonGroup()
+
+    def _on_dynamic_resize(self) -> None:
+        widget = self._button_group.checkedButton()
+        if widget is not None and self._dynamic_button_widget.isAncestorOf(widget):
+            self._dynamic_button_widget.ensureWidgetVisible(widget, 0, 0)
+        if self._orientation == Qt.Orientation.Vertical:
+            self._arrow_widget.setVisible(
+                self._dynamic_button_widget.height()
+                < self._dynamic_button_widget.child_widget().sizeHint().height()
+            )
+        else:
+            self._arrow_widget.setVisible(
+                self._dynamic_button_widget.width()
+                < self._dynamic_button_widget.child_widget().sizeHint().width()
+            )
 
     def add_layout_button(self) -> ToolbarButton:
         """Add a button to the toolbar."""
@@ -212,12 +348,12 @@ class ToolBar(QWidget):
         button.setFixedSize(QSize(ButtonSize, ButtonSize))
         button.setIconSize(QSize(IconSize, IconSize))
         button.setCheckable(True)
-        self._layout_button_group.addButton(button)
+        self._button_group.addButton(button)
         self._dynamic_button_widget.add_item(button)
         return button
 
     def uncheck_layout_buttons(self) -> None:
-        button = self._layout_button_group.checkedButton()
+        button = self._button_group.checkedButton()
         if button is not None:
             button.setChecked(False)
 
