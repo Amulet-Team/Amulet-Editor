8abec95a6f44c810cf41cdc2d21750845090cd06
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-28 16:27:41 +0100

    Redesign command registry

--- src/amulet/app/cli/__init__.py
+++ src/amulet/app/cli/__init__.py
@@ -1,3 +1,2 @@
-from ._args import FullArgs
-from ._command import Command, register_command, unregister_command
+from ._command import Command, register_command, get_commands
 from ._spawn import spawn_process
--- src/amulet/app/cli/_command.py
+++ src/amulet/app/cli/_command.py
@@ -4,68 +4,41 @@ Plugins can register their own launch commands.
 """
 
 from __future__ import annotations
-from argparse import ArgumentParser
-from typing import Any
-from weakref import WeakSet, WeakValueDictionary
-from collections.abc import Callable, Mapping
+from typing import NoReturn
+from collections.abc import Callable
 from dataclasses import dataclass
 from threading import Lock
-from types import MappingProxyType
-
-from ._parser import FullArgs
 
 
 @dataclass(frozen=True, kw_only=True)
 class Command:
     name: str
-    main_func: Callable[[FullArgs], None]
-    init_argparse: Callable[[ArgumentParser], None] | None = None
-    add_parser_kwargs: Mapping[str, Any] = MappingProxyType({})
-
-    def __hash__(self) -> int:
-        return id(self)
+    main_func: Callable[[list[str]], NoReturn]
 
 
 _lock = Lock()
-_commands_set = WeakSet[Command]()
-_commands_map = WeakValueDictionary[str, Command]()
+_commands = dict[str, Command]()
 
 
 def register_command(command: Command) -> None:
-    """
-    Register a command. The name must be unique.
-    The Command object must be kept alive by the caller.
-    """
+    """Register a command. The name must be unique."""
     if not isinstance(command, Command):
         raise TypeError(f"Command {command} is not a Command")
     with _lock:
-        if command.name in _commands_map:
+        if command.name in _commands:
             raise RuntimeError(f"Command {command.name} has already been registered.")
-        _commands_set.add(command)
-        _commands_map[command.name] = command
+        _commands[command.name] = command
 
 
-def unregister_command(command: Command) -> None:
-    """
-    Unregister a command.
-    If the Command object was not previously registered, this will do nothing.
-    """
-    if not isinstance(command, Command):
-        raise TypeError(f"Command {command} is not a Command")
+def get_commands() -> list[str]:
+    """Get the list of command strings."""
     with _lock:
-        if command in _commands_set:
-            _commands_set.remove(command)
-            _commands_map.pop(command.name)
+        return list(_commands)
 
 
-def get_commands() -> list[Command]:
+def run_command(name: str, args: list[str]) -> NoReturn:
     with _lock:
-        return list(_commands_set)
-
-
-def run_command(name: str, args: FullArgs) -> None:
-    with _lock:
-        command = _commands_map.get(name)
+        command = _commands.get(name)
     if command is None:
         raise RuntimeError(f'Could not find the "{name}" command.')
     command.main_func(args)
