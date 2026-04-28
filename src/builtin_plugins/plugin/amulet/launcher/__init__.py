0209eda233d1fd45b34aee6e8e980c0da801c90c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-28 15:50:57 +0100

    Add placeholder launcher command

---
+++ src/builtin_plugins/plugin/amulet/launcher/__init__.py
@@ -0,0 +1,10 @@
+from typing import NoReturn
+
+from amulet.app.cli import register_command, Command
+
+def launcher_command(args: list[str]) -> NoReturn:
+    pass
+
+_launcher_command = Command(name="amulet_launcher", main_func=launcher_command)
+
+register_command(_launcher_command)
