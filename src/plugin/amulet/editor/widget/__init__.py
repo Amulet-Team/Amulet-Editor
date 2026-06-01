b4627ec988461fdc8f19b3f8f08fab6063704c21
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-06-01 13:57:38 +0100

    Remove unregister_tab_widget

--- src/plugin/amulet/editor/widget/__init__.py
+++ src/plugin/amulet/editor/widget/__init__.py
@@ -1,2 +1,2 @@
-from ._widget import register_tab_widget, unregister_tab_widget
+from ._widget import register_tab_widget
 from .abc import TabWidget
--- src/plugin/amulet/editor/widget/_widget.py
+++ src/plugin/amulet/editor/widget/_widget.py
@@ -28,22 +28,6 @@ def register_tab_widget(widget_identifier: str, widget_cls: type[TabWidget]) -> 
         _layout.populate_widgets(widget_identifier, widget_cls)
 
 
-def unregister_tab_widget(widget_identifier: str) -> None:
-    """
-    Unregister a widget class.
-
-    :param widget_identifier: The identifier for the widget type. Eg my_namespace.my_plugin.MyWidget
-    :return:
-    """
-    with lock:
-        if widget_identifier not in _widget_classes:
-            raise ValueError(
-                f"TabWidget type {widget_identifier} has not been registered."
-            )
-        del _widget_classes[widget_identifier]
-        _layout.remove_widgets(widget_identifier)
-
-
 def get_tab_widget_cls(widget_identifier: str) -> type[TabWidget]:
     """Get the registered widget from its qualified name.
 
