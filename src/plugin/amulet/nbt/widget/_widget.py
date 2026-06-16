ab026ce95a131648cfac77587f54dfcd9a660554
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-06-16 16:20:37 +0100

    Move NBT icons to a new package

--- src/plugin/amulet/nbt/widget/_widget.py
+++ src/plugin/amulet/nbt/widget/_widget.py
@@ -1,4 +1,3 @@
-import os
 from copy import deepcopy
 from typing import SupportsInt
 
@@ -45,34 +44,9 @@ from amulet.app.exception import CatchExceptionDialog
 from plugin.tablericons import tablericons
 
 from ._spin import NBTSpinBox
+from ..icon import get_pixmap, get_icon
 
 type TagType = NamedTag | AnyNBT
-
-
-_icons: dict[type[TagType], QPixmap] = {}
-
-
-def get_icon(tag_cls: type[TagType]) -> QPixmap | None:
-    if not _icons:
-        icon_sheet = QPixmap(
-            os.path.realpath(os.path.join(os.path.dirname(__file__), "nbtsheet.png"))
-        )
-        for x, y, cls in [
-            (0, 0, ByteTag),
-            (1, 0, DoubleTag),
-            (2, 0, FloatTag),
-            (3, 0, IntTag),
-            (0, 1, LongTag),
-            (1, 1, ShortTag),
-            (2, 1, StringTag),
-            (3, 1, CompoundTag),
-            (0, 2, ByteArrayTag),
-            (1, 2, IntArrayTag),
-            (2, 2, ListTag),
-            (3, 2, LongArrayTag),
-        ]:
-            _icons[cls] = icon_sheet.copy(32 * x, 32 * y, 32, 32)
-    return _icons.get(tag_cls)
 
 
 def get_string(v: str | bytes) -> str:
@@ -218,7 +192,7 @@ class NBTTreeWidgetItem(QTreeWidgetItem):
 
     def update_icon(self) -> None:
         """Set the icon for this item."""
-        icon = get_icon(type(self._tag))
+        icon = get_pixmap(type(self._tag))
         self.setIcon(0, icon or QPixmap())
 
     def _set_child_policy(self) -> None:
