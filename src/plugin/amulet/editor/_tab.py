a21e8c6800a6e9c7733e1074713fc3b2a204a2cd
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-07-16 09:54:13 +0100

    Allow a tab or tool to reject being closed (#202)
    * Allow a tab or tool to reject being closed
    
    * Update mypy.ini
    
    * Reformat

--- src/plugin/amulet/editor/_tab.py
+++ src/plugin/amulet/editor/_tab.py
@@ -1,7 +1,26 @@
-class WindowTabClose:
-    def close_tab(self) -> bool:
+class ToolAboutToHide:
+    def tool_about_to_hide(self) -> bool:
         """
-        Called just before the tab is closed.
+        Called when the tool is about to be hidden.
+        Return False to prevent the tool being hidden.
+        This is called when switching world tabs, switching tools, or closing the level tab.
+        """
+        return True
+
+
+class TabAboutToHide:
+    def tab_about_to_hide(self) -> bool:
+        """
+        Called when the tab is about to be hidden.
+        Return False to prevent the tab from being hidden.
+        """
+        return True
+
+
+class TabAboutToClose:
+    def tab_about_to_close(self) -> bool:
+        """
+        Called when the tab is about to be closed.
         Return False to prevent the tab from being closed.
         """
         return True
--- src/plugin/amulet/editor/api.py
+++ src/plugin/amulet/editor/api.py
@@ -1,3 +1,4 @@
 """This is the public interface to interact with the editor."""
 
 from ._main_window import get_amulet_editor
+from ._tab import ToolAboutToHide
